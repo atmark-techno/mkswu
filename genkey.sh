5f571e733a511b0cb1ea2c244b7129ca73c6d40e
Dominique Martinet <dominique.martinet@atmark-techno.com>
2021-06-09 16:44:38 +0900

    genkey: allow setting key file encryption

--- genkey.sh
+++ genkey.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 CONFIG=./mkimage.conf
 AES=
+RSA_CIPHER=-aes-256-cbc
 
 error() {
 	local line
@@ -16,18 +17,23 @@ usage() {
 	echo "Usage: $0 [options]"
 	echo
 	echo "Options:"
-	echo "  -c, --config  path"
-	echo "  --aes         generate aes key instead of default rsa key pair"
+	echo "  -c, --config         path"
+	echo "  --aes                generate aes key instead of default rsa key pair"
+	echo "  --rsa-cipher cipher  cipher to use for rsa key encryption"
+	echo "                       (default -aes-256-cbc, set empty for clear text)"
 }
 
 genkey_aes() {
 	local oldumask
 
-	[ -n "$ENCRYPT_KEYFILE" ] || error "Must set ENCRYPT_KEYFILE in config file (or pass --config if required)"
-	[ -e "$ENCRYPT_KEYFILE" ] && error "$ENCRYPT_KEYFILE already exists, aborting"
+	[ -n "$ENCRYPT_KEYFILE" ] \
+		|| error "Must set ENCRYPT_KEYFILE in config file (or pass --config if required)"
+	[ -s "$ENCRYPT_KEYFILE" ] \
+		&& error "$ENCRYPT_KEYFILE already exists, aborting"
 
 	echo "Creating encryption keyfile $ENCRYPT_KEYFILE"
 	echo "That file must be copied over to /etc/swupdate.aes-key as 0400 on boards"
+
 	oldumask=$(umask)
 	umask 0077
 	ENCRYPT_KEY="$(openssl rand -hex 32)" || error "No openssl?"
@@ -40,14 +46,15 @@ genkey_rsa() {
 	local PUBKEY="${PRIVKEY%.key}.pem"
 
 	[ -n "$PRIVKEY" ] || error "PRIVKEY is not set in config file"
-	[ -e "$PRIVKEY" ] && error "$PRIVKEY already exists, skipping"
+	[ -s "$PRIVKEY" ] && error "$PRIVKEY already exists, skipping"
 
 	echo "Creating signing key $PRIVKEY and its public counterpart ${PUBKEY##*/}"
 	echo "$PUBKEY must be copied over to /etc/swupdate.pem on boards"
 
 	oldumask=$(umask)
 	umask 0077
-	openssl genrsa -aes256 -out "$PRIVKEY"
+	openssl genpkey -out "$PRIVKEY" -algorithm rsa-pss \
+		-pkeyopt rsa_keygen_bits:4096 $RSA_CIPHER
 	umask "$oldumask"
 	openssl rsa -in "$PRIVKEY" -out "$PUBKEY" -outform PEM -pubout
 }
@@ -58,6 +65,11 @@ while [ $# -ge 1 ]; do
 		[ $# -lt 2 ] && error "$1 requires an argument"
 		CONFIG="$2"
 		[ "${CONFIG#/}" = "$CONFIG" ] && CONFIG="./$CONFIG"
+		shift 2
+		;;
+	"--rsa-cipher")
+		[ $# -lt 2 ] && error "$1 requires an argument"
+		RSA_CIPHER="$2"
 		shift 2
 		;;
 	"--aes")
