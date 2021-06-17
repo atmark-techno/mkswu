66e8b8d242bf3b45f3ab553ba529e4d9e29eb937
Dominique Martinet <dominique.martinet@atmark-techno.com>
2021-06-17 10:49:25 +0900

    post_rootfs: copy /etc/containers/storage.conf link and atmark/root shadow if set

--- scripts/post_rootfs.sh
+++ scripts/post_rootfs.sh
@@ -1,15 +1,61 @@
+link_exists() {
+	local base="$1"
+	local target="$2"
+
+	if [ "${target:0:1}" = "/" ]; then
+		[ -e "/target/$target" ]
+	else
+		[ -e "/target/$base/$target" ]
+	fi
+}
+
+update_shadow() {
+	local user="$1"
+	local oldpass newpass_lastday
+
+	oldpass=$(awk -F':' '$1 == "'"$user"'" && $3 != "0"' /etc/shadow)
+	# password was never set: skip
+	[ -n "$oldpass" ] || return
+
+	newpass_lastday=$(awk -F':' '$1 == "'"$user"'" { print($3 == "" ? 1 : $3) }' /target/etc/shadow)
+	# user doesn't exist on dest: skip
+	[ -n "$newpass_lastday" ] || return
+	# password already set on dest: skip
+	[ "$newpass_lastday" != 0 ] && return
+
+	sed -i -e 's:^'"$user"'\:.*:'"${oldpass//:/\\:}"':' /target/etc/shadow
+}
+
 post_rootfs() {
+	local storage_conf_link
+
 	# if other fs wasn't up to date: fix partition-specific things
+	# note that this means these files cannot be updated through swupdate
+	# as this script will always reset them.
 	if ! grep -q "other_rootfs_uptodate" "/etc/sw-versions" 2>/dev/null; then
+		# fwenv: either generate a new one for mmc, or copy for sd boot (only one env there)
 		if [ "$mmcblk" = "/dev/mmcblk2" ]; then
 			cat > /target/etc/fw_env.config <<EOF
 ${mmcblk}boot${ab} 0x3fe000 0x2000
 ${mmcblk}boot${ab} 0x3fa000 0x2000
 EOF
+		else
+			cp /etc/fw_env.config /target/etc/fw_env.config
 		fi
 
 		# adjust ab_boot
 		sed -i -e "s/boot_[01]/boot_${ab}/" /target/etc/fstab
+
+		# keep same storage.conf as current if using link
+		if storage_conf_link=$(readlink /etc/containers/storage.conf) \
+		    && [ "$storage_conf_link" != "$(readlink /target/etc/containers/storage.conf)" ] \
+		    && link_exists "/etc/containers" "$storage_conf_link"; then
+			rm -f /target/etc/containers/storage.conf
+			ln -s "$storage_conf_link" /target/etc/containers/storage.conf
+		fi
+
+		update_shadow root
+		update_shadow atmark
 	fi
 
 	# Three patterns:
