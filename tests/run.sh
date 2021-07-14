a39a1b4853f561d6dc7579ca8f1cd90ea633170b
Dominique Martinet <dominique.martinet@atmark-techno.com>
2021-07-14 10:09:46 +0900

    add install test
    actually run swupdate on an image with DEBUG_SKIP_SCRIPTS as last syntax sanity check
    This also checks more than it looks like as some obvious script failures will also be
    detected.

--- tests/run.sh
+++ tests/run.sh
@@ -6,3 +6,16 @@ set -ex
 . ./tests/common.sh
 
 build_check tests/spaces "file container_docker_io_tag_with_spaces.pull"
+build_check tests/install_files
+
+# install test
+SWUPDATE="${SWUPDATE:-swupdate}"
+if command -v "$SWUPDATE" > /dev/null; then
+	rm -rf /tmp/swupdate-test
+	mkdir /tmp/swupdate-test
+	"$SWUPDATE" -i ./tests/out/install_files.swu -v -k swupdate.pem
+	ls /tmp/swupdate-test
+	[ -e "/tmp/swupdate-test/test space" ] || error "Not installed properly"
+	[ -e "/tmp/swupdate-test/test space.tar" ] || error "Not installed properly"
+	rm -rf /tmp/swupdate-test
+fi
