2d7874aa89713f6960404158d3594948b49a427b
Dominique Martinet <dominique.martinet@atmark-techno.com>
2021-07-14 14:54:30 +0900

    jenkins: add yakushima-es1 image

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -4,4 +4,5 @@
 set -ex
 
 ./jenkins/yakushima-eva.sh
+./jenkins/yakushima-es1.sh
 ./tests/run.sh
---
+++ jenkins/yakushima-es1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+error() {
+	echo "$@" >&2
+	exit 1
+}
+
+[[ -r "swupdate.key" ]] || error "Cannot read swupdate.key"
+
+ROOTFS=$(ls --sort=time alpine-aarch64-*.tar* | head -n 1)
+[[ -e  "$ROOTFS" ]] || error "rootfs not found"
+ROOTFS_VERSION=${ROOTFS#alpine-aarch64-}
+ROOTFS_VERSION=${ROOTFS_VERSION%.tar.*}
+# if multiple dashes only keep until first one
+ROOTFS_VERSION=${ROOTFS_VERSION%%-*}
+
+cat > yakushima-es1.desc <<EOF
+swdesc_uboot imx-boot_yakushima-es1
+swdesc_tar "$ROOTFS" --version base_os "$ROOTFS_VERSION"
+EOF
+
+. ./tests/common.sh
+
+build_check "yakushima-es1" "version uboot 20.*" "version base_os .+"
+mv tests/out/yakushima-es1.swu .
