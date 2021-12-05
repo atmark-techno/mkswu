6f51c383348ed3e54da75aa69b6c09f9fb1be942
Dominique Martinet <dominique.martinet@atmark-techno.com>
2021-12-06 08:46:24 +0900

    jenkins: remove trailing words from rootfs version
    this lets mkimage.sh accept test build images with custom suffix

--- jenkins/armadillo-x2.sh
+++ jenkins/armadillo-x2.sh
@@ -14,6 +14,12 @@ ROOTFS_VERSION=${ROOTFS_VERSION%.tar.*}
 
 OUTPUT=baseos-x2-${ROOTFS_VERSION}
 
+# cleanup version for test builds entries: remove trailing words
+while [[ "$ROOTFS_VERSION" =~ -at.*-[a-z] ]]; do
+	ROOTFS_VERSION=${ROOTFS_VERSION%-[a-z]*}
+done
+
+
 cat > "$OUTPUT.desc" <<EOF
 DEBUG_SWDESC="# ALLOW_PUBLIC_CERT ALLOW_EMPTY_LOGIN"
 swdesc_boot --board iot-g4-eva imx-boot_yakushima-eva
