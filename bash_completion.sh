d63797e81c6ef05ac4527c9dbb855d047c922455
Dominique Martinet <dominique.martinet@atmark-techno.com>
2021-12-28 14:30:39 +0900

    bash_completion: fix podman_partial_image completion with colons
    - podman_partial_image was removed from this repo but will be installed,
      its completion probably ought to follow but keep it here for simplicity
      and just adjust its path for now...
    - adding colon to COMP_WORDBREAKS was a bad idea, assume it's not present
      and work around it by manually trimming pre-colon values
    - complete localhost/image names too

--- bash_completion.sh
+++ bash_completion.sh
@@ -18,27 +18,33 @@ _mkimage_sh()
 } && complete -F _mkimage_sh ./mkimage.sh
 
 
-# remove : from wordbreaks as a workaround for silly readline behaviour
-# see https://tiswww.case.edu/php/chet/bash/FAQ
-# E13) Why does filename completion misbehave if a colon appears in the filename?
-COMP_WORDBREAKS=${COMP_WORDBREAKS//:}
-
 _podman_image_list()
 {
-	podman image list --format '{{range .Names}}{{.}}{{println}}{{end}}{{.Id}}' \
-		| sed -e 's:^localhost/::'
+	local filter
+	filter="${cur%:*}"
+	if [ "$filter" != "$cur" ]; then
+		podman image list --format '{{range .Names}}{{.}}{{println}}{{end}}' "$filter" \
+			| awk -F: '$1 == "'"$filter"'" { print $2 }'
+	else
+		podman image list --format '{{range .Names}}{{.}}{{println}}{{end}}{{.Id}}' \
+			| sed -e 's:^localhost/\(.*\):\1\n&:'
+	fi
 }
-_podman_partial_image_sh()
+_podman_partial_image()
 {
-	local cur prev words cword split
-	_init_completion -n ':' || return
+	local cur curcol prev words cword split image
+	# We do not split colons here to have sensible prev/cur variable,
+	# but we are expected to only fill in whatever is after the colon
+	# in COMPREPLY so care must be taken after on...
+	_init_completion -n ":" || return
+	curcol=${cur##*:}
 	case "$prev" in
 	-o|--output)
 		_filedir
 		return
 		;;
 	-b|--base)
-		COMPREPLY=($(compgen -W "$(_podman_image_list)" -- "$cur"))
+		COMPREPLY=($(compgen -W "$(_podman_image_list)" -- "$curcol"))
 		return
 		;;
 	-R|--rename)
@@ -47,13 +53,12 @@ _podman_partial_image_sh()
 	esac
 
 	if [[ $cur == -* ]]; then
-		COMPREPLY=($(compgen -W "-b --base -o --output -R --rename" -- "$cur"))
+		COMPREPLY=($(compgen -W "-b --base -o --output -R --rename" -- "$curcol"))
 		return
 	fi
 
-	COMPREPLY=($(compgen -W "$(_podman_image_list)" -- "$cur"))
-	__ltrim_colon_completions
-} && complete -F _podman_partial_image_sh ./podman_partial_image.sh
+	COMPREPLY=($(compgen -W "$(_podman_image_list)" -- "$curcol"))
+} && complete -F _podman_partial_image podman_partial_image
 
 _hawkbit_create-update_sh()
 {
