6a5c707b9d88c73c27d77ff1d62f05ddae7260d4
Dominique Martinet <dominique.martinet@atmark-techno.com>
2022-01-04 14:09:12 +0900

    init.sh: add new script to make initial setup more straightforward
    - generates keys if required
    - walks through initial_setup.desc configurations

--- Makefile
+++ Makefile
@@ -1,7 +1,22 @@
+# could be possible to handle wildcard of languages later
+# https://stackoverflow.com/a/40881718
+
+l = ja
+translations = $(wildcard po/$(l)/*.po)
+locales = $(patsubst po/$(l)/%.po,locale/$(l)/LC_MESSAGES/%.mo,$(translations))
+
+pot = $(wildcard po/*.pot)
+
+all: $(locales) $(pot)
+
+
 po/mkimage.pot: mkimage.sh examples/enable_sshd.desc examples/hawkbit_register.desc
 	sed -e 's/info "\|error "/$$"/' < mkimage.sh | bash --dump-po-strings | sed -e 's/bash:/mkimage.sh:/' > po/mkimage.pot
 	sed -e 's/info "\|error "/$$"/' < examples/enable_sshd.desc | bash --dump-po-strings | sed -e 's/bash:/enable_sshd.desc:/' >> po/mkimage.pot
 	sed -e 's/info "\|error "/$$"/' < examples/hawkbit_register.desc | bash --dump-po-strings | sed -e 's/bash:/hawkbit_register.desc:/' >> po/mkimage.pot
 
-locale/ja/LC_MESSAGES/mkimage.mo: po/ja/mkimage.po
+po/init.pot: init.sh
+	bash --dump-po-strings $< > $@
+
+locale/$(l)/LC_MESSAGES/%.mo: po/$(l)/%.po
 	msgfmt -o $@ $<
---
+++ init.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$0")"
+CONFIG="$SCRIPT_DIR/mkimage.conf"
+TEXTDOMAINDIR="$SCRIPT_DIR/locale"
+TEXTDOMAIN=init
+
+error() {
+	printf "ERROR: %s\n" "$@" >&2
+	exit 1
+}
+info() {
+	printf "%s\n" "$@" >&2
+}
+
+genkey() {
+	local CN
+
+	[[ -e "$PUBKEY" ]] && [[ -e "$PRIVKEY" ]] && return
+
+	while [[ -z "$CN" ]]; do
+		read -r -p $"Enter certificate common name: " CN
+	done
+
+	"$SCRIPT_DIR/genkey.sh" --cn "$CN" || error $"Could not generate swupdate certificate"
+
+	# Also prompt for encryption
+	local AES=inval
+
+	[[ -n "$ENCRYPT_KEYFILE" ]] && [[ -e "$ENCRYPT_KEYFILE" ]] && return
+
+	while ! [[ "$AES" =~ ^([Yy]|[Yy][Ee][Ss]|[Nn]|[Nn][Oo]|)$ ]]; do
+		read -r -p $"Use AES encryption? (N/y) " AES
+	done
+	case "$AES" in
+	[Yy]|[Yy][Ee][Ss])
+		"$SCRIPT_DIR/genkey.sh" --aes
+		;;
+	esac
+}
+
+geninitdesc() {
+	local KEEPATMARKPEM=inval
+	local ROOTPW
+	local ROOTPW_CONFIRM
+	local ATMARKPW
+	local ATMARKPW_CONFIRM
+	local desc="$SCRIPT_DIR/initial_setup.desc"
+
+	[[ -e "$desc" ]] && return
+
+	while ! [[ "$KEEPATMARKPEM" =~ ^([Yy]|[Yy][Ee][Ss]|[Nn]|[Nn][Oo]|)$ ]]; do
+		read -r -p $"Allow updates signed by Atmark Techno? (Y/n) " KEEPATMARKPEM
+	done
+	while true; do
+		read -s -r -p $"root password: " ROOTPW
+		echo
+		if [[ -z "$ROOTPW" ]]; then
+			info $"A root password is required"
+			continue
+		fi
+		read -s -r -p $"root password (confirm): " ROOTPW_CONFIRM
+		echo
+		if [[ "$ROOTPW" != "$ROOTPW_CONFIRM" ]]; then
+			info $"Passwords do not match"
+			continue
+		fi
+		ROOTPW=$(printf 'import crypt; print(crypt.crypt(r"%s", crypt.METHOD_SHA512))' "${ROOTPW//\"/\"\'\"\'r\"}" | python3)
+		[[ -n "$ROOTPW" ]] || error $"Could not hash password"
+		break
+	done
+
+	while true; do
+		read -s -r -p $"atmark user password (empty = same as root): " ATMARKPW
+		echo
+		read -s -r -p $"atmark user password (confirm): " ATMARKPW_CONFIRM
+		echo
+		if [[ "$ATMARKPW" != "$ATMARKPW_CONFIRM" ]]; then
+			info $"Passwords do not match"
+			continue
+		fi
+		if [[ -z "$ATMARKPW" ]]; then
+			ATMARKPW="$ROOTPW_CONFIRM"
+		fi
+		ATMARKPW=$(printf 'import crypt; print(crypt.crypt(r"%s", crypt.METHOD_SHA512))' "${ATMARKPW//\"/\"\'\"\'r\"}" | python3)
+		[[ -n "$ATMARKPW" ]] || error $"Could not hash password"
+		break
+	done
+
+
+	# cleanup if we fail here
+	trap "rm -f ${desc@Q}" EXIT
+
+	cp "$SCRIPT_DIR/examples/initial_setup.desc" "$desc" \
+		|| error $"Could not copy initial_setup.desc from example dir"
+	case "$KEEPATMARKPEM" in
+	[Nn]|[Nn][Oo])
+		sed -i -e 's@>> /etc/swupdate.pem@> /etc/swupdate.pem@' "$desc" \
+			|| error $"Could not update $desc"
+	esac
+	sed -i -e 's:\(^[ \t]*"usermod\).*atmark:\1 -p '\'\""'$ATMARKPW'"\"\'' atmark:' \
+			-e 's:\(^[ \t]*"usermod\).*root:\1 -p '\'\""'$ROOTPW'"\"\'' root:' "$desc" \
+		|| error $"Could not update $desc"
+
+	trap "" EXIT
+}
+
+mkimageinitswu() {
+	"$SCRIPT_DIR/mkimage.sh" "$SCRIPT_DIR/initial_setup.desc" || error $"Could not generate initial setup swu"
+	echo
+	info $"You can use \"$SCRIPT_DIR/initial_setup.swu\" as is or regenerate an image"
+	info $"with extra modules with \"$SCRIPT_DIR/mkimage.sh\" \"$SCRIPT_DIR/initial_setup.desc\" other_desc_files"
+}
+
+
+if ! [[ -r "$CONFIG" ]]; then
+        # generate defaults if absent
+        [[ "${CONFIG##*/}" = "mkimage.conf" ]] \
+                && "$SCRIPT_DIR/mkimage.sh" --mkconf
+        [[ -r "$CONFIG" ]] \
+                || error $"Config $CONFIG not found - configure paths there or specify config with --config"
+fi
+. "$CONFIG"
+
+genkey
+geninitdesc
+mkimageinitswu
+
+
