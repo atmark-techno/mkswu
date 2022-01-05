atmark-techno/mkswu

commit 1aacd26f0cad4ab755b41c119bbf80205dea8904

scripts: remove versions include in post phase
versions scripts are complex and we do not need them for post phase.
Move a few helper functions and roll without it

scripts/common.sh
@@ -136,6 +136,14 @@ remove_bootdev_link() {
 	rm -f /dev/swupdate_bootdev
 }
 
+needs_reboot() {
+	[ -n "$needs_reboot" ]
+}
+
+update_rootfs() {
+	[ -n "$update_rootfs" ]
+}
+
 cleanup() {
 	remove_bootdev_link
 	umount_if_mountpoint /target/var/lib/containers/storage_readonly/overlay

scripts/post_rootfs.sh
@@ -129,6 +129,23 @@ update_swupdate_certificate()  {
 	rm -rf "$certsdir"
 }
 
+update_running_versions() {
+	cp "$1" /etc/sw-versions || error "Could not update /etc/sw-versions"
+
+	[ "$(stat -f -c %T /etc/sw-versions)" = "overlayfs" ] || return
+
+	# support older version of overlayfs
+	local fsroot=/live/rootfs
+	[ -e "$fsroot" ] || fsroot=/
+
+	# bind-mount / somewhere else to write below it as well
+	mount --bind "$fsroot" /target || error "Could not bind mount rootfs"
+	mount -o remount,rw /target || error "Could not make rootfs rw"
+	cp /etc/sw-versions /target/etc/sw-versions \
+		|| error "Could not write $1 to /etc/sw-versions"
+	umount /target || error "Could not umount rootfs rw copy"
+}
+
 overwrite_to_target() {
 	local file
 	local dir

scripts/versions.sh
@@ -77,14 +77,6 @@ needs_update_regex() {
 	return 1
 }
 
-needs_reboot() {
-	[ -n "$needs_reboot" ]
-}
-
-update_rootfs() {
-	[ -n "$update_rootfs" ]
-}
-
 extract_swdesc_versions() {
 	# extract version comments
 	sed -ne "s/.*#VERSION //p"
@@ -120,21 +112,4 @@ gen_newversion() {
 		oldvers=$(get_version "$component" "$system_versions")
 		[ -z "$oldvers" ] && printf "%s\n" "$component $newvers"
 	done < "$SCRIPTSDIR/sw-versions.present" >> "$SCRIPTSDIR/sw-versions.merged"
-}
-
-update_running_versions() {
-	cp "$1" /etc/sw-versions || error "Could not update /etc/sw-versions"
-
-	[ "$(stat -f -c %T /etc/sw-versions)" = "overlayfs" ] || return
-
-	# support older version of overlayfs
-	local fsroot=/live/rootfs
-	[ -e "$fsroot" ] || fsroot=/
-
-	# bind-mount / somewhere else to write below it as well
-	mount --bind "$fsroot" /target || error "Could not bind mount rootfs"
-	mount -o remount,rw /target || error "Could not make rootfs rw"
-	cp /etc/sw-versions /target/etc/sw-versions \
-		|| error "Could not write $1 to /etc/sw-versions"
-	umount /target || error "Could not umount rootfs rw copy"
 }

swupdate_post.sh
@@ -4,7 +4,6 @@ TMPDIR="${TMPDIR:-/var/tmp}"
 SCRIPTSDIR="$TMPDIR/scripts"
 
 . "$SCRIPTSDIR/common.sh"
-. "$SCRIPTSDIR/versions.sh"
 
 . "$SCRIPTSDIR/post_init.sh"
 . "$SCRIPTSDIR/post_appfs.sh"

commit 33bf8e1c81d8e0b3a2f5ecdbff2fc8d9e7834783

scripts: introduce 'update_baseos' check for post_rootfs
post_rootfs still used needs_update but the function was no longer
available since versions.sh was no longer included in post phase

Fixes: 1aacd26f0cad ("scripts: remove versions include in post phase")

scripts/common.sh
@@ -144,6 +144,10 @@ update_rootfs() {
 	[ -n "$update_rootfs" ]
 }
 
+update_baseos() {
+	[ "$update_rootfs" = "baseos" ]
+}
+
 cleanup() {
 	remove_bootdev_link
 	umount_if_mountpoint /target/var/lib/containers/storage_readonly/overlay

scripts/post_init.sh
@@ -17,8 +17,9 @@ init() {
 		needs_reboot=1
 	fi
 
-	if [ -e "$SCRIPTSDIR/update_rootfs" ]; then
-		update_rootfs=1
+	if [ -f "$SCRIPTSDIR/update_rootfs" ]; then
+		update_rootfs=$(cat "$SCRIPTSDIR/update_rootfs") \
+			|| error "Could not read update_rootfs variable but file existed?!"
 	fi
 }
 

scripts/post_rootfs.sh
@@ -230,7 +230,7 @@ EOF
 				/target/etc/containers/storage.conf \
 				|| error "could not rewrite storage.conf"
 		fi
-		if needs_update "base_os"; then
+		if update_baseos; then
 			baseos_upgrade_fixes
 			post_copy_preserve_files
 		fi

scripts/pre_init.sh
@@ -36,7 +36,9 @@ init_vars() {
 	# non-fatal if not present
 	board=$(awk '{print $1; exit}' /etc/hwrevision 2>/dev/null)
 
-	if needs_update base_os || needs_update_regex "extra_os.*" \
+	if needs_update base_os; then
+		update_rootfs=baseos
+	elif needs_update_regex "extra_os.*" \
 	    || grep -q "CONTAINER_CLEAR" "$SWDESC"; then
 		update_rootfs=1
 	fi
@@ -55,7 +57,7 @@ save_vars() {
 			|| error "Could not save need to reboot variable"
 	fi
 	if update_rootfs; then
-		touch "$SCRIPTSDIR/update_rootfs" \
+		echo "$update_rootfs" > "$SCRIPTSDIR/update_rootfs" \
 			|| error "Could not save rootfs update variable"
 	fi
 }