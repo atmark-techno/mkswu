6ab049c760f9f90248cfcd45719c9a961f8a75b9
Dominique Martinet <dominique.martinet@atmark-techno.com>
2022-01-11 15:12:16 +0900

    bash_completion: move in a subdir and split commands
    installing bash completion requires one file per command.
    command names and hook don't really match what we have in repo, so
    this will be fixed when command names get cast in stone for installed
    version and running in repo will be left as an exercice for the user
    if required

---
+++ bash_completion.d/mkimage.sh
@@ -0,0 +1,18 @@
+_mkimage_sh()
+{
+	local cur prev words cword split
+	_init_completion || return
+	case "$prev" in
+	-o|--out)
+		_filedir
+		return
+		;;
+	-c|--config)
+		_filedir 'conf'
+		return
+		;;
+	esac
+
+	COMPREPLY=($(compgen -W '-o --out -c --config' -- "$cur"))
+	_filedir 'desc'
+} && complete -F _mkimage_sh mkimage.sh
--- bash_completion.d/podman_partial_image.sh
+++ bash_completion.d/podman_partial_image.sh
@@ -1,23 +1,3 @@
-_mkimage_sh()
-{
-	local cur prev words cword split
-	_init_completion || return
-	case "$prev" in
-	-o|--out)
-		_filedir
-		return
-		;;
-	-c|--config)
-		_filedir 'conf'
-		return
-		;;
-	esac
-
-	COMPREPLY=($(compgen -W '-o --out -c --config' -- "$cur"))
-	_filedir 'desc'
-} && complete -F _mkimage_sh ./mkimage.sh
-
-
 _podman_image_list()
 {
 	local filter
@@ -59,12 +39,3 @@ _podman_partial_image()
 
 	COMPREPLY=($(compgen -W "$(_podman_image_list)" -- "$curcol"))
 } && complete -F _podman_partial_image podman_partial_image
-
-_hawkbit_create-update_sh()
-{
-	local cur prev words cword split
-	_init_completion || return
-	COMPREPLY=($(compgen -W '--new --failed --start --no-rollout --keep-tmpdir' -- "$cur"))
-	_filedir 'swu'
-} && complete -F _hawkbit_create-update_sh ./create-update.sh \
-  && complete -F _hawkbit_create-update_sh ./hawkbit/create-update.sh
