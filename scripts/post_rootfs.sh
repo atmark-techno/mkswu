atmark-techno/mkswu

commit 272264cc1aec3b0648225b2960f03a1d09f8d4b1

scripts: rework post_rootfs logic
since updating containers now defaults to rebooting, add a new
flag that checks if rootfs has been updated separately.
The rework also fixes the rootfs up to date logic that would
clear the rootfs up to date flag on container update

scripts/post_rootfs.sh
@@ -65,17 +65,16 @@ update_shadow() {
 }
 
 post_rootfs() {
-	local storage_conf_link
 	# Sanity check: refuse to continue if someone tries to write a
 	# rootfs that was corrupted or "too wrong": check for /bin/sh
 	if ! [ -e /target/bin/sh ]; then
 		error "No /bin/sh on target: something likely is wrong with rootfs, refusing to continue"
 	fi
 
-	# if other fs wasn't up to date: fix partition-specific things
+	# if other fs was updated: fix partition-specific things
 	# note that this means these files cannot be updated through swupdate
 	# as this script will always reset them.
-	if ! grep -q "other_rootfs_uptodate" "/etc/sw-versions" 2>/dev/null; then
+	if update_rootfs || ! grep -q "other_rootfs_uptodate" "/etc/sw-versions" 2>/dev/null; then
 		# fwenv: either generate a new one for mmc, or copy for sd boot (only one env there)
 		if [ "$rootdev" = "/dev/mmcblk2" ]; then
 			cat > /target/etc/fw_env.config <<EOF
@@ -88,38 +87,45 @@ EOF
 
 		# adjust ab_boot
 		sed -i -e "s/boot_[01]/boot_${ab}/" /target/etc/fstab
+	fi
 
-		# keep same storage.conf as current if using link
-		if storage_conf_link=$(readlink /etc/containers/storage.conf) \
-		    && [ "$storage_conf_link" != "$(readlink /target/etc/containers/storage.conf)" ] \
-		    && link_exists "/etc/containers" "$storage_conf_link"; then
-			rm -f /target/etc/containers/storage.conf
-			ln -s "$storage_conf_link" /target/etc/containers/storage.conf
+	# copy a few more files from previous rootfs on update...
+	if update_rootfs; then
+		# use appfs storage for podman if used previously
+		if grep -q 'graphroot = "/var/lib/containers/storage' /etc/containers/storage.conf 2>/dev/null; then
+			sed -i -e 's@graphroot = .*@graphroot = "/var/lib/containers/storage"@' \
+				/target/etc/containers/storage.conf
 		fi
 
+		# keep passwords around, and make sure there are no open access user left
 		update_shadow
 	fi
 
-	# Three patterns:
-	# - we didn't update rootfs, but other rootfs wasn't up to date yet
-	#   * set flag in both version files
-	# - we didn't update rootfs, other rootfs already up to date
-	#   * other rootfs untouched, update version files to current partition
-	# - we wrote some data and need rebooting:
-	#   * rootfs uptodate flag was cleared by pre script
-	#   * update version files to target partition
-	if ! needs_reboot; then
-		# if we're not rebooting, only update current versions and mark
-		# the other rootfs up to date
-		if ! grep -q "other_rootfs_uptodate" "/etc/sw-versions"; then
-			echo "other_rootfs_uptodate 1" >> /target/etc/sw-versions
-			echo "other_rootfs_uptodate 1" >> "$SCRIPTSDIR/sw-versions.merged"
+	# set other_rootfs_uptodate flag on both sides if appropriate
+	if ! update_rootfs && ! grep -q "other_rootfs_uptodate" "/etc/sw-versions"; then
+		echo "other_rootfs_uptodate 1" >> "$SCRIPTSDIR/sw-versions.merged"
+		if needs_reboot; then
+			cp /etc/sw-versions "$SCRIPTSDIR/sw-versions.uptodate"
+			echo "other_rootfs_uptodate 1" >> "$SCRIPTSDIR/sw-versions.uptodate"
+			update_running_versions "$SCRIPTSDIR/sw-versions.uptodate"
+			rm "$SCRIPTSDIR/sw-versions.uptodate"
+		else
+			echo "other_rootfs_uptodate 1" >> /target/etc/sw-versions \
+				|| error "Could not write to /target/etc/sw-versions"
 		fi
-		
-		update_running_versions "$SCRIPTSDIR/sw-versions.merged"
 	else
-		grep -v "other_rootfs_uptodate" "$SCRIPTSDIR/sw-versions.merged" > \
-			"/target/etc/sw-versions"
+		sed -i -e "/other_rootfs_uptodate/d" "$SCRIPTSDIR/sw-versions.merged"
+		# has already been cleared on running system in pre_rootfs
+	fi
+
+	# and finally set version where appropriate.
+	if ! needs_reboot; then
+		# updating current version with what is being installed:
+		# we should avoid failing from here on.
+		update_running_versions "$SCRIPTSDIR/sw-versions.merged"
+		soft_fail=1
+	else
+		cp "$SCRIPTSDIR/sw-versions.merged" "/target/etc/sw-versions"
 	fi
 
 

scripts/common.sh
@@ -1,6 +1,12 @@
 error() {
 	echo "$@" >&2
 	cleanup
+	if [ -n "$soft_fail" ]; then
+		echo "An error happened after changes have been applied" >&2
+		echo "This most likely means success status cannot be reported correctly." >&2
+		echo "Reboot to finish applying anything left" >&2
+		reboot
+	fi
 	exit 1
 }
 

scripts/post_appfs.sh
@@ -43,7 +43,7 @@ cleanup_appfs() {
 
 	btrfs property set -ts /target/var/lib/containers/storage_readonly ro true
 
-	if grep -q 'graphroot = "/var/lib/containers/storage' /target/etc/containers/storage.conf 2>/dev/null; then
+	if grep -q 'graphroot = "/var/lib/containers/storage' /etc/containers/storage.conf 2>/dev/null; then
 		# make sure mount point exists in destination image
 		mkdir -p /target/var/lib/containers/storage
 	fi
@@ -59,7 +59,7 @@ cleanup_appfs() {
 			echo "Could not swap btrfs subvolumes, forcing reboot"
 			umount "$basemount"
 			rmdir "$basemount"
-			force_reboot=1
+			needs_reboot=1
 			return
 		fi
 		umount "$basemount"

scripts/post_init.sh
@@ -16,6 +16,10 @@ init() {
 	if [ -e "$SCRIPTSDIR/needs_reboot" ]; then
 		needs_reboot=1
 	fi
+
+	if [ -e "$SCRIPTSDIR/update_rootfs" ]; then
+		update_rootfs=1
+	fi
 }
 
 init

scripts/post_uboot.sh
@@ -15,16 +15,22 @@ cleanup_uboot() {
 			|| error "Could not flip mmc boot flag"
 	elif [ -s /etc/fw_env.config ]; then
 		# if uboot env is supported, use it (e.g. sd card)
-		fw_setenv mmcpart $((ab+1))
+		fw_setenv mmcpart $((ab+1)) \
+			|| error " Could not setenv mmcpart"
 	elif [ -e /target/boot/extlinux.conf ]; then
 		# assume gpt boot e.g. extlinux
-		sgdisk --attributes=$((ab+1)):set:2 --attributes=$((!ab+1)):clear:2 "$rootdev"
+		sgdisk --attributes=$((ab+1)):set:2 --attributes=$((!ab+1)):clear:2 "$rootdev" \
+			|| error "Could not set boot attribute"
 
-		sed -i -e "s/root=[^ ]*/root=LABEL=rootfs_${ab}/" /target/boot/extlinux.conf
+		sed -i -e "s/root=[^ ]*/root=LABEL=rootfs_${ab}/" /target/boot/extlinux.conf \
+			|| error "Could not update extlinux.conf"
 		extlinux -i /target/boot || error "Could not reinstall bootloader"
 	else
 		error "Do not know how to A/B switch this system"
 	fi
+
+	# from here on, failure is not appropriate
+	soft_fail=1
 }
 
 cleanup_uboot

scripts/pre_appfs.sh
@@ -38,7 +38,7 @@ prepare_appfs() {
 		mount "$dev" "$basemount" || error "Could not mount $dev"
 	fi
 
-	if grep -q 'graphroot = "/var/lib/containers/storage' /target/etc/containers/storage.conf 2>/dev/null; then
+	if grep -q 'graphroot = "/var/lib/containers/storage' /etc/containers/storage.conf 2>/dev/null; then
 		echo "Persistent storage is used for podman, stopping all containers before taking snapshot" >&2
 		echo "This is only for development, do not use this mode for production!" >&2
 		podman kill -a

scripts/pre_init.sh
@@ -33,8 +33,11 @@ init_vars() {
 		probe_current
 	fi
 
-	if needs_update uboot || needs_update base_os \
-	    || needs_update kernel || needs_update_regex "extra_os.*" \
+	if needs_update base_os || needs_update kernel \
+	    || needs_update_regex "extra_os.*"; then
+		update_rootfs=1
+	fi
+	if [ -n "$rootfs_update" ] || needs_update uboot \
 	    || ! grep -q "NO_REBOOT_ALLOW" "$SWDESC"; then
 		needs_reboot=1
 	fi
@@ -48,7 +51,11 @@ save_vars() {
 		|| error "Could not save local variables"
 	if needs_reboot; then
 		touch "$SCRIPTSDIR/needs_reboot" \
-			|| error "Could not save need to reboot"
+			|| error "Could not save need to reboot variable"
+	fi
+	if update_rootfs; then
+		touch "$SCRIPTSDIR/update_rootfs" \
+			|| error "Could not save rootfs update variable"
 	fi
 }
 

scripts/pre_rootfs.sh
@@ -45,15 +45,16 @@ prepare_rootfs() {
 	# If there is no need to reboot, we can use it -- otherwise we need
 	# to clear the flag.
 	if grep -q other_rootfs_uptodate /etc/sw-versions 2>/dev/null; then
-		if ! needs_reboot; then
-			echo "Other fs up to date, mounting read-only"
-			mount "$dev" "/target" -o ro || error "Could not mount $dev"
+		if ! rootfs_updated; then
+			echo "Other fs up to date, skipping copy"
+			mount "$dev" "/target" || error "Could not mount $dev"
 			return
 		fi
-		echo "Clearing other fs up to date flag"
+		echo "Clearing other fs up to date flag and reformatting it"
 		grep -v 'other_rootfs_uptodate' /etc/sw-versions \
-			> "$TMPDIR/sw-versions.nouptodate"
-		update_running_versions "$TMPDIR/sw-versions.nouptodate"
+			> "$SCRIPTSDIR/sw-versions.nouptodate"
+		update_running_versions "$SCRIPTSDIR/sw-versions.nouptodate"
+		rm "$SCRIPTSDIR/sw-versions.nouptodate"
 	fi
 
 	# check if partitions exist and create them if not:

commit 6e7a5f0965ef6f1972fb9743efd4fb541ccb66dd

post_rootfs cleanup: remove unused link_exists function
Fixes: 272264cc1aec ("scripts: rework post_rootfs logic")

scripts/post_rootfs.sh
@@ -1,14 +1,3 @@
-link_exists() {
-	local base="$1"
-	local target="$2"
-
-	if [ "${target:0:1}" = "/" ]; then
-		[ -e "/target/$target" ]
-	else
-		[ -e "/target/$base/$target" ]
-	fi
-}
-
 update_shadow_user() {
 	local user="$1"
 	local oldpass