atmark-techno/mkswu

commit e4b00b9e0f4b17c9ace7200c224fa10815697b6f

mkswu: add prompt_yesno helper

mkswu
@@ -79,6 +79,23 @@ prompt() {
 	fi
 }
 
+prompt_yesno() {
+	local default="$1"
+	shift 1
+	local yesno
+
+	while true; do
+		prompt yesno "$@"
+		[ -n "$yesno" ] || yesno="$default"
+		case "$yesno" in
+		[Yy]|[Yy][Ee][Ss]|[Tt][Rr][Uu][Ee]|1)
+			return 0;;
+		[Nn]|[Nn][Oo]|[Ff][Aa][Ll][Ss][Ee]|0)
+			return 1;;
+		esac
+	done
+}
+
 usage() {
 	info "Usage: %s [opts] desc [desc...]" "$0"
 	info
@@ -1562,9 +1579,9 @@ mkinit_genkey() {
 
 	if [ -n "$KEYPASS" ]; then
 		echo "$KEYPASS" | PRIVKEY_PASS="stdin" \
-			VERBOSE=1 mkswu_genkey_sign
+			VERBOSE=1 NOPROMPT=1 mkswu_genkey_sign
 	else
-		GENKEY_PLAIN=1 VERBOSE=1 mkswu_genkey_sign
+		GENKEY_PLAIN=1 VERBOSE=1 NOPROMPT=1 mkswu_genkey_sign
 	fi || exit 1
 
 	# Also prompt for encryption
@@ -1572,14 +1589,7 @@ mkinit_genkey() {
 
 	[ -n "$ENCRYPT_KEYFILE" ] && [ -e "$ENCRYPT_KEYFILE" ] && return
 
-	while true; do
-		prompt AES "Use AES encryption? (N/y)"
-		case "$AES" in
-		[Yy]|[Yy][Ee][Ss]|[Tt][Rr][Uu][Ee]|1) AES=1; break;;
-		[Nn]|[Nn][Oo]|[Ff][Aa][Ll][Ss][Ee]|0|"") AES=""; break;;
-		esac
-	done
-	if [ -n "$AES" ]; then
+	if prompt_yesno n "Use AES encryption? (N/y)"; then
 		VERBOSE=1 mkswu_genkey_aes
 		info "Generated %s" "$ENCRYPT_KEYFILE"
 	fi
@@ -1630,28 +1640,6 @@ geninitdesc_promptpass() {
 	PASSWD="$password"
 }
 
-geninitdesc_prompt_autoupdate() {
-	while true; do
-		prompt AUTOUPDATE "Enable auto-update of BaseOS image from armadillo.atmark-techno.com servers? (N/y)"
-		case "$AUTOUPDATE" in
-		[Yy]|[Yy][Ee][Ss]|[Tt][Rr][Uu][Ee]|1)
-			break;;
-		[Nn]|[Nn][Oo]|[Ff][Aa][Ll][Ss][Ee]|0|"")
-			AUTOUPDATE=""; return;;
-		esac
-	done
-
-	while true; do
-		prompt AUTOUPDATE "Select update frequency ([weekly]/daily)"
-		case "$AUTOUPDATE" in
-		weekly|"")
-			AUTOUPDATE="next week"; break;;
-		daily)
-			AUTOUPDATE="tomorrow"; break;;
-		esac
-	done
-}
-
 geninitdesc_fixinitdesc() {
 	local version
 	version=$(sed -ne 's/.*version=\([0-9]*\).*/\1/p' "$desc")
@@ -1687,25 +1675,12 @@ mkinit_geninitdesc() {
 
 	[ -e "$desc" ] && geninitdesc_fixinitdesc && return
 
-	while true; do
-		prompt KEEPATMARKPEM "Allow updates signed by Atmark Techno? (Y/n)"
-		case "$KEEPATMARKPEM" in
-		[Yy]|[Yy][Ee][Ss]|[Tt][Rr][Uu][Ee]|1|"")
-			KEEPATMARKPEM=1; break;;
-		[Nn]|[Nn][Oo]|[Ff][Aa][Ll][Ss][Ee]|0)
-			KEEPATMARKPEM=""; break;;
-		esac
-	done
+	prompt_yesno y "Allow updates signed by Atmark Techno? (Y/n)" && KEEPATMARKPEM=1
 
 	geninitdesc_promptpass "root"
 	ROOTPW="$PASSWD"
 	geninitdesc_promptpass "atmark"
 	ATMARKPW="$PASSWD"
-
-	if [ -n "$KEEPATMARKPEM" ]; then
-		geninitdesc_prompt_autoupdate
-	fi
-
 
 	# cleanup if we fail here
 	trap "rm -f $desc.tmp" EXIT
@@ -1721,7 +1696,18 @@ mkinit_geninitdesc() {
 			-e 's:\(^[ \t]*"usermod\).*root:\1 '"$ROOTPW"' root:' "$desc.tmp" \
 		|| error "Could not update %s" "$desc.tmp"
 
-	if [ -n "$AUTOUPDATE" ]; then
+	if [ -n "$KEEPATMARKPEM" ] && \
+	    prompt n "Enable auto-update of BaseOS image from armadillo.atmark-techno.com servers? (N/y)"; then
+
+		while true; do
+			prompt AUTOUPDATE "Select update frequency ([weekly]/daily)"
+			case "$AUTOUPDATE" in
+			weekly|"")
+				AUTOUPDATE="next week"; break;;
+			daily)
+				AUTOUPDATE="tomorrow"; break;;
+			esac
+		done
 		cat >> "$desc.tmp" <<EOF \
 			|| error "Could not update %s" "$desc.tmp"
 
@@ -1859,6 +1845,7 @@ sw-description.sig"
 	# mode options
 	local GENKEY_PLAIN="" GENKEY_CN=""
 	local SHOW_RAW=""
+	local NOPROMPT=""
 
 	# default default values
 	local BOOT_SIZE="4M"
@@ -2007,6 +1994,10 @@ sw-description.sig"
 			[ $# -lt 1 ] && error "%s requires an argument" "$ARG"
 			GENKEY_CN="$1"
 			SKIP=1
+			;;
+		"--noprompt")
+			[ "$MODE" = "genkey_sign" ] || error "%s must be passed after %s" "$ARG" "--genkey"
+			NOPROMPT=1
 			;;
 		"--version")
 			info "mkswu version %s" "$MKSWU_VERSION"

tests/run.sh
@@ -11,7 +11,7 @@ if [ "$CLEAN_TESTS_OUT" = "yes" ]; then
 	rm -rf ./out
 fi
 
-"${MKSWU:-../mkswu}" --genkey --cn test --plain
+"${MKSWU:-../mkswu}" --genkey --cn test --plain --noprompt
 
 . ./common.sh
 ./examples.sh

commit b2c90aa748ceda5458238724c17f47ce94143a4f

mkswu --init: fix enabling autoupdate even when refusing
when adding the prompt_yesno helper it was actually not used by
mistake for prompting for autoupdate, leading to autoupdate always
being enabled...

Also add tests that would have catched this to prevent future
regressions...

Fixes: e4b00b9e0f4b ("mkswu: add prompt_yesno helper") # 4.3
Change-Id: I8c16d6fc8a6411aa53c1a66478c2beb55695bbae

mkswu
@@ -1837,7 +1837,7 @@ mkinit_geninitdesc() {
 		|| error "Could not update %s" "$desc.tmp"
 
 	if [ -n "$KEEPATMARKPEM" ] && \
-	    prompt n "Enable auto-update of BaseOS image from armadillo.atmark-techno.com servers? (N/y)"; then
+	    prompt_yesno n "Enable auto-update of BaseOS image from armadillo.atmark-techno.com servers? (N/y)"; then
 
 		while true; do
 			prompt AUTOUPDATE "Select update frequency ([weekly]/daily)"

tests/build_tests.sh
@@ -70,7 +70,7 @@ scripts_pre.sh.zst
 hardlink
 scripts_post.sh.zst" ] || error "cpio content was not in expected order: $(cpio --quiet -t < out/hardlink_order.swu)"
 
-rm -rf "$TESTS_DIR/out/init"
+rm -rf "$TESTS_DIR/out/init" "$TESTS_DIR/out/init_noupdate" "$TESTS_DIR/out/init_noatmark"
 "$MKSWU" --config-dir "$TESTS_DIR/out/init" --init <<EOF \
 	|| error "mkswu --init failed"
 cn
@@ -78,6 +78,8 @@ privkeypass
 privkeypass
 
 
+somepass
+differentpass(ask again)
 root
 root
 atmark
@@ -85,17 +87,70 @@ atmark
 y
 
 EOF
+"$MKSWU" --config-dir "$TESTS_DIR/out/init_noupdate" --init <<EOF \
+	|| error "mkswu --init noupdate failed"
+
+cn(empty=should reask)
+somepass
+differentpass(ask again)
+
+
+
+
+root
+root
+somepass
+differentpass(ask again)
+
+
+
+EOF
+"$MKSWU" --config-dir "$TESTS_DIR/out/init_noatmark" --init <<EOF \
+	|| error "mkswu --init noatmark failed"
+cn
+keypass
+keypass
+y
+n
+
+root
+root
+
+
+EOF
 # in order:
 # certif common name, private key pass x2, aes encryption (default=n), allow atmark updates (default=y),
 # root pass x2, atmark pass x2, autoupdate (force y) + frequency (default weekly)
+# frequency skipped if autoupdate = n (default)
+
+grep -q swupdate-url "$TESTS_DIR/out/init/initial_setup.desc" \
+	|| error "autoupdate not enabled"
+grep -q swupdate-url "$TESTS_DIR/out/init_noupdate/initial_setup.desc" \
+	&& error "autoupdate incorrectly enabled (noupdate)"
+grep -q swupdate-url "$TESTS_DIR/out/init_noatmark/initial_setup.desc" \
+	&& error "autoupdate incorrectly enabled (noatmark)"
+grep -qxF "swdesc_command '> /etc/swupdate.pem'" \
+		"$TESTS_DIR/out/init_noatmark/initial_setup.desc" \
+	|| error "noatmark kept atmark certs"
+grep -qxF "swdesc_command '> /etc/swupdate.pem'" \
+		"$TESTS_DIR/out/init/initial_setup.desc" \
+	&& error "incorrectly wiped atmark certs"
 
 # validate generated passwords match
 checkpass() {
-	local user="$1"
-	local pass="$2"
+	local dir="$1"
+	local user="$2"
+	local pass="$3"
 	local desc
 	local check
-	desc=$(sed -ne "s/^[ \t].*'\"'\(.*\)'\"'.*$user.*/\1/p" "$TESTS_DIR/out/init/initial_setup.desc")
+
+	if [ -z "$pass" ]; then
+		grep -qE "^[^#]*\"usermod -L $user\"" "$TESTS_DIR/out/$dir/initial_setup.desc" \
+			|| error "$user not locked in $dir"
+		return
+	fi
+
+	desc=$(sed -ne "s/^[^#]*'\"'\(.*\)'\"'.*$user.*/\1/p" "$TESTS_DIR/out/$dir/initial_setup.desc")
 	if command -v python3 >/dev/null; then
 		check=$(python3 -c "import crypt; print(crypt.crypt('$pass', '$desc'))") \
 			|| error "python3 crypt call failed"
@@ -107,10 +162,12 @@ checkpass() {
 	else
 		error "install either python3 or mkpasswd"
 	fi
-	[ "$desc" = "$check" ] || error "Error: $pass was invalid (got $desc expected $check)"
+	[ "$desc" = "$check" ] || error "Error: $pass was invalid (got $desc expected $check for $dir)"
 }
-checkpass atmark atmark
-checkpass root root
+checkpass init atmark atmark
+checkpass init root root
+checkpass init_noupdate atmark
+checkpass init_noupdate root root
 
 # test atmark pass is regenerated on old version
 sed -i -e 's/version=[0-9]/version=1/' "$TESTS_DIR/out/init/initial_setup.desc"