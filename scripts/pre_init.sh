atmark-techno/mkswu

commit abfbab5f41a04a395561162faacd098d580d610c

scripts: do not mount B-side if upgrade_available is unset
We've removed the scary messages with a previous commit but it is more
correct to not try to mount the other partition if update isn't set.

This doesn't fix anything obvious, but e.g. would allow install to pass
if power dropped after sw-versions have been written to B-side and
before u-boot got switched, so is probably worth doing.

Change-Id: Ib6b39062c2943caba0346540934b235f58d27880

scripts/pre_init.sh
@@ -51,6 +51,10 @@ init_vars_update() {
 	if [ "$POST_ACTION" != "container" ]; then
 		needs_reboot=1
 	fi
+	if [ -e "/etc/fw_env.config" ] \
+	    && fw_printenv upgrade_available | grep -qx 'upgrade_available=1'; then
+		upgrade_available=1
+	fi
 }
 
 save_vars() {
@@ -68,6 +72,8 @@ save_vars() {
 }
 
 fail_redundant_update() {
+	[ -z "$upgrade_available" ] && return
+
 	# if no version changed, clean up and fail script to avoid
 	# downloading the rest of the image
 	if [ -z "$(mkswu_var FORCE_VERSION)" ]; then

scripts/pre_rootfs.sh
@@ -191,7 +191,8 @@ mount_target_rootfs() {
 
 	# Check if the current copy is up to date.
 	# If there is no base_os update we can use it.
-	if ! needs_update "base_os" \
+	if [ -n "$upgrade_available" ] \
+	    && ! needs_update "base_os" \
 	    && luks_unlock "rootfs_$ab" \
 	    && mount -t ext4,btrfs "$dev" /target 2>/dev/null; then
 		if [ ! -e /target/.created ] \

commit 6963c38f7dac5dec967761b53531eb28be7a7d7d

scripts/pre_init: fix version check without upgrade_available
The commit disabling B-side mount when upgrade_available is unset was
incorrect in that it also disabled the "useless version" check.

This would cause an update to be installed again immediately after
initial install, or in the worst case an install loop if rollback is
explicitly disabled and an update is provided by USB.

Reported-by: Naomi TODORI <naomi.todori@atmark-techno.com>
Fixes: abfbab5f41a0 ("scripts: do not mount B-side if upgrade_available is unset")
Change-Id: I4e0163bfd948e0eae6bca64f48aad00544fbc3b1

scripts/pre_init.sh
@@ -72,8 +72,6 @@ save_vars() {
 }
 
 fail_redundant_update() {
-	[ -z "$upgrade_available" ] && return
-
 	# if no version changed, clean up and fail script to avoid
 	# downloading the rest of the image
 	if [ -z "$(mkswu_var FORCE_VERSION)" ]; then
@@ -90,6 +88,7 @@ fail_redundant_update() {
 		# also check B-side unless SW_ALLOW_ROLLBACK is set
 		local dev="${partdev}$((ab+1))"
 		if [ -z "$SW_ALLOW_ROLLBACK" ] \
+		    && [ -n "$upgrade_available" ] \
 		    && luks_unlock "rootfs_$ab" \
 		    && mount -t ext4,btrfs "$dev" /target 2>/dev/null; then
 			if cmp -s /target/etc/sw-versions \