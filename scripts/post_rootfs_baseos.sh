atmark-techno/mkswu

commit 895535753b26cd98e53c96cebc13adf43bee7a75

scripts/post_rootfs_baseos: add missing /target prefixes
a few files were checking the old (current) rootfs instead of install
target's, fix all usages to use /target/...

Change-Id: I957bb55956b4ec84b1dafe7eac502f8a8c433da9

scripts/post_rootfs_baseos.sh
@@ -96,7 +96,7 @@ EOF
 	# (added in 3.16.2-at.6)
 	case " $overlays " in
 	*" armadillo_iotg_g4-aw-xm458.dtbo "*)
-		if ! [ -e /etc/init.d/wifi-recover ]; then
+		if ! [ -e /target/etc/init.d/wifi-recover ]; then
 			enable_service wifi-recover default
 		fi
 		;;
@@ -104,10 +104,10 @@ EOF
 
 	# correct URL in /etc/swupdate.watch
 	local target_for_x2="https://download.atmark-techno.com/armadillo-iot-g4/image/baseos-x2-latest.swu"
-	if [ "$(cat /etc/swupdate.watch)" = "$target_for_x2" ]; then
+	if [ "$(cat /target/etc/swupdate.watch)" = "$target_for_x2" ]; then
 		case "$(cat /etc/hwrevision)" in
-		iot-a6e*) sed -i -e 's/g4/a6e/; s/x2/6e/' /etc/swupdate.watch;;
-		AX2210*) sed -i -e 's/iot-g4/x2/' /etc/swupdate.watch;;
+		iot-a6e*) sed -i -e 's/g4/a6e/; s/x2/6e/' /target/etc/swupdate.watch;;
+		AX2210*) sed -i -e 's/iot-g4/x2/' /target/etc/swupdate.watch;;
 		esac || error "Could not update swupdate.watch"
 	fi
 }

commit 55d97b942b7e83b44f29598bc9c4b74d11e5f6dd

scripts/post_rootfs_baseos: remove wifi-recover
Previous commit adding /target to files incorrectly added it for
wifi-recover: the intent was to enable the service if it was not present
before.
But wifi-recover never actually got distributed so we can just remove
this section.

Also silence a few more harmless error messages when files do not exist.

Change-Id: I68ae9c9fe1abed834ab00da0efa897cb524ca05b
Fixes: 895535753b26 ("scripts/post_rootfs_baseos: add missing /target prefixes")

scripts/post_rootfs_baseos.sh
@@ -33,7 +33,7 @@ baseos_upgrade_fixes() {
 
 	# note this is the currently running version,
 	# not the version we install (which would always be too recent!)
-	baseos_version=$(cat /etc/atmark-release) || return
+	baseos_version=$(cat /etc/atmark-release 2>/dev/null) || return
 
 	# not a baseos install? skip fixes...
 	[ -n "$baseos_version" ] || return
@@ -91,20 +91,10 @@ EOF
 		enable_service wwan-led default
 		;;
 	esac
-	# Enable wifi-recover, only for wifi-enabled boards if the
-	# service just got added to allow for users disabling it.
-	# (added in 3.16.2-at.6)
-	case " $overlays " in
-	*" armadillo_iotg_g4-aw-xm458.dtbo "*)
-		if ! [ -e /target/etc/init.d/wifi-recover ]; then
-			enable_service wifi-recover default
-		fi
-		;;
-	esac
 
 	# correct URL in /etc/swupdate.watch
 	local target_for_x2="https://download.atmark-techno.com/armadillo-iot-g4/image/baseos-x2-latest.swu"
-	if [ "$(cat /target/etc/swupdate.watch)" = "$target_for_x2" ]; then
+	if [ "$(cat /target/etc/swupdate.watch 2>/dev/null)" = "$target_for_x2" ]; then
 		case "$(cat /etc/hwrevision)" in
 		iot-a6e*) sed -i -e 's/g4/a6e/; s/x2/6e/' /target/etc/swupdate.watch;;
 		AX2210*) sed -i -e 's/iot-g4/x2/' /target/etc/swupdate.watch;;