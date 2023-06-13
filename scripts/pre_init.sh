edb2018deab6904ef93f3d3c42baa36d92ff5afa
Dominique Martinet <dominique.martinet@atmark-techno.com>
2023-06-13 17:38:23 +0900

    versions: drop other_boot/other_boot_linux
    the whole system with other_boot was complex and ultimately not
    sufficient when failed updates or rollback was invovled: drop the
    variables
    
    When sufficiently enough time passed we can also drop the 'drop the
    variable' code, but there is no hurry there.
    
    Change-Id: Icf2ed76527107a0e7234a8df191eb1ce0cf7c5ed

--- scripts/pre_init.sh
+++ scripts/pre_init.sh
@@ -75,13 +75,10 @@ fail_redundant_update() {
 	# if no version changed, clean up and fail script to avoid
 	# downloading the rest of the image
 	if [ -z "$(mkswu_var FORCE_VERSION)" ]; then
-		# exclude other_boot and other_boot_linux from comparison,
-		# otherwise update with nothing to do will trigger another update for these.
 		# create sw-versions if it didn't exist so diff doesn't complain
 		[ -e /etc/sw-versions ] || touch /etc/sw-versions
 		if ! diff -U0 /etc/sw-versions "$SCRIPTSDIR/sw-versions.merged" \
-				| tail -n +3 | grep -E "^[+-]" \
-				| grep -qvE "^[+-]other_boot |^[+-]other_boot_linux "; then
+				| tail -n +3 | grep -qE "^[+-]"; then
 			rm -rf "$SCRIPTSDIR"
 			error "Nothing to do -- failing on purpose to save bandwidth"
 		fi
