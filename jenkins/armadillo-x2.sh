atmark-techno/mkswu

commit 3d384c082493b7f3b333079451a8ea28f3806757

mkswu: allow multiple swdesc_exec on a file if different command
Calling swdesc_script with different arguments would fail due to the same
script being send with the same name multiple times.
Fix this by hashing the command into the filename in the cpio archive.

Also fix show_cmd for swdesc_exec that was missing filename.

Reported-by: Akihito Irie <akihito.irie@atmark-techno.com>
Change-Id: Iae43f0fb3306049dedadae4d39cdf9163bca902b

mkswu
@@ -636,7 +636,7 @@ parse_swdesc() {
 		for ARG; do
 			cmd="${cmd:+$cmd && }$ARG"
 		done
-		[ -n "$show_cmd" ] || show_cmd="swdesc_$CMD $cmd"
+		[ -n "$show_cmd" ] || show_cmd="swdesc_$CMD $file $cmd"
 		;;
 	*container)
 		[ $# -eq 0 ] && [ -n "$image" ] && return
@@ -880,7 +880,7 @@ conf_quote() {
 }
 
 swdesc_exec_nochroot() {
-	local file="$file" cmd="$cmd"
+	local file="$file" cmd="$cmd" orig_file
 	local component="$component" version="$version"
 	local board="$board" main_version="$main_version"
 	local install_if="$install_if" show_cmd="$show_cmd"
@@ -891,6 +891,14 @@ swdesc_exec_nochroot() {
 	[ ! -s "$file" ] || [ "$cmd" != "${cmd#*\$1}" ] \
 		|| error 'Using swdesc_exec_nochroot with a non-empty file, but not referring to it with $1'
 
+	# allow multiple execs with same file if command changes
+	# don't rename if no arguments for easier debugging
+	if [ -n "$cmd" ] && [ "$cmd" != 'sh $1 ' ]; then
+		orig_file="$file"
+		set_file_from_content "$file" "$cmd"
+		link "$orig_file" "$file"
+	fi
+
 	cmd="sh -c $(shell_quote "$cmd") --"
 
 	write_entry files "$file" "type = \"exec\";" \
@@ -899,7 +907,7 @@ swdesc_exec_nochroot() {
 }
 
 swdesc_exec() {
-	local file="$file" cmd="$cmd" chroot_cmd
+	local file="$file" cmd="$cmd" chroot_cmd orig_file
 	local component="$component" version="$version"
 	local board="$board" main_version="$main_version"
 	local install_if="$install_if" show_cmd="$show_cmd"
@@ -925,6 +933,14 @@ swdesc_exec() {
 		chroot_cmd="$chroot_cmd --rootfs /target sh -c $(shell_quote "$cmd") -- "
 		;;
 	esac
+
+	# allow multiple execs with same file if command changes
+	# don't rename if no arguments for easier debugging
+	if [ -n "$cmd" ] && [ "$cmd" != 'sh $1 ' ]; then
+		orig_file="$file"
+		set_file_from_content "$file" "$cmd"
+		link "$orig_file" "$file"
+	fi
 
 	write_entry files "$file" "type = \"exec\";" \
 		"properties: {" \

tests/build_tests.sh
@@ -39,6 +39,9 @@ printf "%s\n" "swdesc_files build_tests.sh --version stdin 1" \
 build_check swdesc_script.desc --
 build_check swdesc_script_nochroot.desc --
 
+build_check two_scripts.desc -- \
+	"swdesc 'one' 'two'"
+
 build_check update_certs_atmark.desc -- \
 	"file-tar scripts_extras.tar certs_atmark/atmark-1.pem certs_atmark/atmark-2.pem"
 

tests/examples.sh
@@ -8,8 +8,8 @@ cd "$(dirname "$0")"
 . ./common.sh
 
 # custom script: no prereq
-build_check ../examples/custom_script.desc -- "file custom_script_app.sh scripts_pre.sh.zst" \
-	"swdesc scripts_pre.sh.zst custom_script.app.sh 'POST_ACTION container' 'Built with mkswu [0-9]'"
+build_check ../examples/custom_script.desc -- "file scripts_pre.sh.zst" \
+	"swdesc custom_script_app.sh scripts_pre.sh.zst custom_script.app.sh 'POST_ACTION container' 'Built with mkswu [0-9]'"
 
 # sshd: build tar
 build_check ../examples/enable_sshd.desc -- "version extra_os.sshd '[^ ]+ higher'" \
@@ -38,7 +38,7 @@ build_check ../examples/kernel_update_plain.desc -- \
 
 # kernel apk: likewise we don't actually test install here,
 touch ../examples/linux-at-5.10.9-r3.apk
-build_check ../examples/kernel_update_apk.desc -- "file linux-at-5.10.9-r3.apk" "swdesc linux-at-5.10.9-r3.apk"
+build_check ../examples/kernel_update_apk.desc -- "swdesc linux-at-5.10.9-r3.apk"
 
 # volume files: relative, absolute path
 build_check ../examples/volumes_assets.desc -- \

tests/run.sh
@@ -118,6 +118,13 @@ line2 with space and ! \ @" ] || error "updated content does not match (podman)"
 line1
 line2 with space and ! \ @" ] || error "updated content does not match (nochroot)"
 	rm -rf /tmp/swupdate-test
+
+	mkdir /tmp/swupdate-test
+	"$SWUPDATE" -i ./out/two_scripts.swu -v -k ../swupdate.pem \
+		|| error "swupdate failed"
+	[ "$(cat /tmp/swupdate-test/echo)" = "one
+two" ] || error "updated content does not match (two scripts)"
+	rm -rf /tmp/swupdate-test
 fi
 
 # finish with a successful command to not keep last failed on purpose test result

tests/two_scripts.desc
@@ -1,0 +1,10 @@
+FORCE_VERSION=1
+DEBUG_SWDESC="# DEBUG_SKIP_SCRIPTS"
+
+cat > "$OUTDIR/echo.sh" <<EOF
+#!/bin/sh
+echo "\$@" >> /tmp/swupdate-test/echo
+EOF
+
+swdesc_script_nochroot "$OUTDIR/echo.sh" -- one
+swdesc_script_nochroot "$OUTDIR/echo.sh" -- two

commit cc58841e8d3162c158abf93fae27e7adfbc6cc4f

swdesc_*: try to preserve file extension
Some commands like apk will fail if their target hasn't preserved their
extension.
There was two places where we mangle file names:
- in swdesc_exec itself, on non-trivial commands we regenerate a
filename with a unique name based on command content.
In this case just carry the last component around if it is short.
- in write_entry_stdout when compressing or encrypting the file.
Prepend zst or enc instead of appending it.

For swdesc_exec the only reason we do the rename is to allow multiple
identical commands, and we could skip the first rename e.g. only do
rename after we detect a collision, but due to compression/encryption
rename it is not obvious at this stage and there doesn't seem to be
much use of trying too hard.

A better solution would be to have swdesc_exec take a new parameter and
expand the file with the original name on device, but since that will
require a new swupdate version it will take time to be effective.

Reported-by: Akihito Irie <akihito.irie@atmark-techno.com>
Change-Id: I178722284906be39c94ed235ae839d091c74eaf3
Fixes: 3d384c082493 ("mkswu: allow multiple swdesc_exec on a file if different command")

jenkins/armadillo-x2.sh
@@ -34,7 +34,7 @@ EOF
 
 build_check "$OUTPUT.desc" -- "version --board AGX4500 boot '20[^ ]* different'" \
 	"version base_os '[^ ]+ higher'" \
-	"file imx-boot_armadillo_x2.* '$ROOTFS'" \
+	"file .*imx-boot_armadillo_x2 '$ROOTFS'" \
 	"file-tar scripts_extras.tar certs_atmark/atmark-1.pem certs_atmark/atmark-2.pem" \
 	"swdesc imx-boot_armadillo_x2 '$ROOTFS' '# MKSWU_ALLOW_PUBLIC_CERT 1'"
 

mkswu
@@ -461,8 +461,8 @@ write_entry_stdout() {
 				compress=""
 			else
 				compress=zstd
-				file="$file.zst"
-				file_out="$file_out.zst"
+				file="zst.$file"
+				file_out="$OUTDIR/$file"
 				compress "$file_src" "$file_out"
 				file_src="$file_out"
 			fi
@@ -471,8 +471,8 @@ write_entry_stdout() {
 	fi
 
 	if [ -n "$ENCRYPT_KEY" ] && [ -s "$file_src" ]; then
-		file="$file.enc"
-		file_out="$file_out.enc"
+		file="enc.$file"
+		file_out="$OUTDIR/$file"
 		# if we already had encrypted this file we must reuse the same iv
 		if [ -e "$OUTDIR/sw-description-ivs" ]; then
 			local tmp
@@ -930,14 +930,21 @@ swdesc_tar() {
 }
 
 set_file_from_content() {
-	local content="$*"
+	local content="$*" extension=""
+	# some commands like apk require extension to be kept to work
+	case "$file" in
+	*.*)
+		extension="${file##*.}"
+		[ "${#extension}" -gt 5 ] && extension=""
+		;;
+	esac
 
 	file="$(printf %s "$content" | tr -c '[:alnum:]' '_')"
 	if [ "${#file}" -gt 40 ]; then
 		file="$(printf %s "$file" | head -c 20)..$(printf %s "$file" | tail -c 20)"
 	fi
 	file="${file}_$(printf %s "$content" | sha1sum | cut -d' ' -f1)"
-	file="$OUTDIR/${file}"
+	file="$OUTDIR/${file}${extension:+.$extension}"
 }
 
 create_archive() {

tests/build_tests.sh
@@ -6,15 +6,15 @@ cd "$(dirname "$0")"
 
 . ./common.sh
 
-build_check spaces.desc -- "file test\ space.tar.zst" \
-	"file .*test_space_tar.*target_load.*\.zst" \
+build_check spaces.desc -- "file zst.test\ space.tar" \
+	"file zst\..*test_space_tar.*target_load.*" \
 	"swdesc 'path = \"/tmp/test space\"'"
 name="--odd desc" build_check --- --odd\ desc.desc
 # --- is made into -- for mkswu
-[ "$(grep -c 'filename = "zoo' "out/.--odd desc/sw-description")" = 1 ] \
+[ "$(grep -c 'filename = "zst.zoo' "out/.--odd desc/sw-description")" = 1 ] \
 	|| error "zoo archive was not included exactly once"
 build_check install_files.desc -- \
-	"file-tar ___tmp_swupdate_test*.tar.zst zoo/test\ space zoo/test\ space.tar" \
+	"file-tar zst.___tmp_swupdate_test*.tar zoo/test\ space zoo/test\ space.tar" \
 	"swdesc '# MKSWU_FORCE_VERSION 1'"
 
 [ -e out/mkswu-aes.conf ] || touch out/mkswu-aes.conf
@@ -29,10 +29,10 @@ build_check install_files.desc -- \
 		|| error "mkswu --genkey --aes failed"
 
 	build_check aes.desc --config out/mkswu-aes.conf -- \
-		"swdesc 'ivt ='" "file scripts_pre.sh.zst.enc"
+		"swdesc 'ivt ='" "file enc.zst.scripts_pre.sh"
 ) || exit
 MKSWU_ENCRYPT_KEYFILE=$PWD/out/swupdate.aes-key build_check aes.desc -- \
-	"swdesc 'ivt ='" "file scripts_pre.sh.zst.enc"
+	"swdesc 'ivt ='" "file enc.zst.scripts_pre.sh"
 
 # test old variables backwards compatibility
 printf "%s\n" "component=test" "install_if=different" "version=1" \
@@ -102,9 +102,9 @@ ln zoo/hardlink zoo/hardlink2
 build_check hardlink_order.desc --
 [ "$(cpio --quiet -t < out/hardlink_order.swu)" = "sw-description
 sw-description.sig
-scripts_pre.sh.zst
+zst.scripts_pre.sh
 hardlink
-scripts_post.sh.zst" ] || error "cpio content was not in expected order: $(cpio --quiet -t < out/hardlink_order.swu)"
+zst.scripts_post.sh" ] || error "cpio content was not in expected order: $(cpio --quiet -t < out/hardlink_order.swu)"
 
 build_check cmd_description.desc -- \
 	"swdesc '# mkswu_orig_cmd swdesc_command_nochroot --description' \

tests/examples.sh
@@ -8,21 +8,21 @@ cd "$(dirname "$0")"
 . ./common.sh
 
 # custom script: no prereq
-build_check ../examples/custom_script.desc -- "file scripts_pre.sh.zst" \
-	"swdesc custom_script_app.sh scripts_pre.sh.zst custom_script.app.sh 'POST_ACTION container' 'Built with mkswu [0-9]'"
+build_check ../examples/custom_script.desc -- "file zst.scripts_pre.sh" \
+	"swdesc custom_script_app.sh zst.scripts_pre.sh custom_script.app.sh 'POST_ACTION container' 'Built with mkswu [0-9]'"
 
 # sshd: build tar
 build_check ../examples/enable_sshd.desc -- "version extra_os.sshd '[^ ]+ higher'" \
-	"file-tar enable_sshd*.tar.zst ./.ssh/authorized_keys" \
-	"swdesc ssh-keygen enable_sshd.*tar.zst rc-update"
+	"file-tar zst.enable_sshd*.tar ./.ssh/authorized_keys" \
+	"swdesc ssh-keygen zst.enable_sshd.*tar rc-update"
 
 # pull container: build tar
 tar -C ../examples/nginx_start -cf ../examples/nginx_start.tar .
 build_check ../examples/pull_container_nginx.desc -- \
-	"file-tar nginx_start*.tar.zst ./etc/atmark/containers/nginx.conf" \
+	"file-tar zst.nginx_start*.tar ./etc/atmark/containers/nginx.conf" \
 	"version pull_container_nginx '[^ ]+ higher'" \
 	"version extra_os.pull_container_nginx '[^ ]+ higher'" \
-	"swdesc nginx_start.**tar.zst docker.io/nginx"
+	"swdesc zst.nginx_start.**tar docker.io/nginx"
 
 # boot: bundle boot image
 if ! [ -e ../imx-boot_armadillo_x2 ] \
@@ -36,14 +36,14 @@ if ! [ -e ../imx-boot_armadillo_x2 ] \
 		echo '2020.04-at2-2-g16be576a6d2a-00001-ge7d8a230e98e'
 	} > ../imx-boot_armadillo_x2
 fi
-build_check ../examples/boot.desc -- "file imx-boot_armadillo_x2.*.zst" "version boot '202.* different'" "swdesc imx-boot_armadillo_x2"
+build_check ../examples/boot.desc -- "file zst.imx-boot_armadillo_x2.*" "version boot '202.* different'" "swdesc imx-boot_armadillo_x2"
 
 # kernel plain: just a couple of files.. since we don't actually check installation create dummy ones
 mkdir -p ../examples/kernel/lib/modules/5.10.82
 touch ../examples/kernel/Image ../examples/kernel/armadillo_iotg_g4.dtb
 touch ../examples/kernel/armadillo_iotg_g4-nousb.dtbo
 build_check ../examples/kernel_update_plain.desc -- \
-	"file-tar *boot_Image*dtb*.tar.zst Image armadillo_iotg_g4.dtb" \
+	"file-tar zst.*boot_Image*dtb*.tar Image armadillo_iotg_g4.dtb" \
 	"swdesc update_preserve_files"
 
 # encrypted linux
@@ -65,8 +65,8 @@ build_check ../examples/kernel_update_apk.desc -- "swdesc linux-at-5.10.9-r3.apk
 
 # volume files: relative, absolute path
 build_check ../examples/volumes_assets.desc -- \
-	"file-tar enable_sshd*tar.zst ./root/.ssh/authorized_keys" \
-	"file-tar __assets_*tar.zst volumes_assets.desc" \
+	"file-tar zst.enable_sshd*tar ./root/.ssh/authorized_keys" \
+	"file-tar zst.__assets_*tar volumes_assets.desc" \
 	"swdesc /var/app/rollback/volumes/assets /var/app/volumes/data"
 
 # no content but force version