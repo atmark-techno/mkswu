atmark-techno/mkswu

commit 721a0d8aa50316cd29f29af5f24909ee7643cf5f

scripts/post_success: keep sw-versions around in /var/log/swupdate
This will be used in abos-web to display rollback informations in a later
update

Change-Id: I93cbee3d6e12e53c4faf2c066a9a6c14275dce06

scripts/post_success.sh
@@ -2,6 +2,11 @@ post_success_rootfs() {
 	# record last updated partition for abos-ctrl
 	local newstate
 
+	if ! [ -d "/var/log/swupdate" ] || ! mkdir /var/log/swupdate; then
+		warning "Could not mkdir /var/log/swupdate"
+		return
+	fi
+
 	if needs_reboot; then
 		newstate="${partdev}$((ab+1))"
 	else
@@ -10,6 +15,8 @@ post_success_rootfs() {
 
 	echo "$newstate $(date +%s)" > "/var/log/swupdate/last_update" \
 		|| warning "Could not record last update partition"
+	cp "/target/etc/sw-versions" "/var/log/swupdate/sw-versions-${partdev#/dev/}" \
+		|| warning "Could not update latest sw-versions"
 }
 
 post_success_atlog() {
@@ -117,8 +124,6 @@ set_fw_update_ind() {
 }
 
 post_success() {
-	[ -d "/var/log/swupdate" ] || mkdir /var/log/swupdate \
-		|| warning "Could not mkdir /var/log/swupdate"
 	post_success_rootfs
 	post_success_atlog
 	[ -n "$SWUPDATE_HAWKBIT" ] && post_success_hawkbit

commit c4b64a1d3b36cb1dab9078a1709980ab9aac89be

scripts/post_success: fix sw-versions copy in /var/log/swupdate
- negating logic requires changing 'or's into 'and's...
- /target is no longer mounted on success, use copy in scripts dir
- when not rebooting, we need to shuffle the old sw-versions around as
containers will have been swapped

Fixes: 721a0d8aa503 ("scripts/post_success: keep sw-versions around in /var/log/swupdate")
Change-Id: Id81b453e4504ec3854d06506f5a2e0632a3019ef

scripts/post_success.sh
@@ -2,7 +2,7 @@ post_success_rootfs() {
 	# record last updated partition for abos-ctrl
 	local newstate
 
-	if ! [ -d "/var/log/swupdate" ] || ! mkdir /var/log/swupdate; then
+	if ! [ -d "/var/log/swupdate" ] && ! mkdir /var/log/swupdate; then
 		warning "Could not mkdir /var/log/swupdate"
 		return
 	fi
@@ -11,11 +11,17 @@ post_success_rootfs() {
 		newstate="${partdev}$((ab+1))"
 	else
 		newstate="${partdev}$((!ab+1))"
+		# no reboot means we updated other partition to our's, first.
+		if [ -e "/var/log/swupdate/sw-versions-${newstate#/dev/}" ]; then
+			mv "/var/log/swupdate/sw-versions-${newstate#/dev/}" \
+					"/var/log/swupdate/sw-versions-${partdev#/dev/}$((ab+1))" \
+				|| warning "Could not update latest sw-versions"
+		fi
 	fi
 
 	echo "$newstate $(date +%s)" > "/var/log/swupdate/last_update" \
 		|| warning "Could not record last update partition"
-	cp "/target/etc/sw-versions" "/var/log/swupdate/sw-versions-${partdev#/dev/}" \
+	cp "$SCRIPTSDIR/sw-versions.merged" "/var/log/swupdate/sw-versions-${newstate#/dev/}" \
 		|| warning "Could not update latest sw-versions"
 }
 