atmark-techno/mkswu

commit 193d9a81e0ad9442bad2459e15c50b6a6a05bbca

scripts: copy swupdate-preserve-files from rootfs
Having some files that were not stored with preserve_files be copied when an
update happens is surprising and not the intended behaviour.

Make sure we copy things from /live/rootfs and not from the overlay

Change-Id: Icda1f344a4f8af30c092f4f308b585a155ec0a53

scripts/pre_rootfs.sh
@@ -12,7 +12,7 @@ copy_to_target() {
 
 		dir="${file%/*}"
 		mkdir_p_target "$dir"
-		cp -aTn "$file" "$TARGET/$file" \
+		cp -aTn "$fsroot$file" "$TARGET/$file" \
 			|| error "Failed to copy $file from previous rootfs"
 	done
 }
@@ -176,6 +176,10 @@ mount_target_rootfs() {
 	local tmp fail extlinux
 	local encrypted=""
 	local fstype=""
+
+	# support older version of overlayfs
+	local fsroot=/live/rootfs/
+	[ -e "$fsroot" ] || fsroot="/"
 
 	cryptsetup isLuks "$dev" >/dev/null 2>&1 && encrypted=1
 

scripts/post_common.sh
@@ -36,11 +36,7 @@ update_user_groups() {
 
 update_shadow() {
 	local user group
-
-	# support older version of overlayfs
-	local fsroot=/live/rootfs
-	[ -e "$fsroot" ] || fsroot=""
-
+	# test constants
 	local PASSWD="${PASSWD:-$fsroot/etc/passwd}"
 	local NPASSWD="${NPASSWD:-/target/etc/passwd}"
 	local SHADOW="${SHADOW:-$fsroot/etc/shadow}"
@@ -77,13 +73,13 @@ update_shadow() {
 }
 
 
-SWUPDATE_PEM=/target/etc/swupdate.pem
-
 update_swupdate_certificate()  {
 	local certsdir cert pubkey
 	local public_onetime_path=""
 	local atmark_present="" atmark_seen=""
 	local user_present="" user_seen=""
+	# test constants
+	local SWUPDATE_PEM=${SWUPDATE_PEM:-/target/etc/swupdate.pem}
 
 	# what certificates were embedded into swu, if any?
 	for cert in "$SCRIPTSDIR/certs_atmark/"*; do

scripts/post_init.sh
@@ -1,5 +1,8 @@
 # Do minimal work if called from installer
 post_installer() {
+	# for update_shadow compat, always mounted in installer
+	local fsroot=/live/rootfs
+
 	# update_shadow checks user password has been set unless made optional
 	update_shadow
 	# update swupdate certificate and versions

scripts/post_rootfs.sh
@@ -3,10 +3,6 @@ update_running_versions() {
 
 	[ "$(stat -f -c %T /etc/sw-versions)" = "overlayfs" ] || return
 
-	# support older version of overlayfs
-	local fsroot=/live/rootfs
-	[ -e "$fsroot" ] || fsroot=/
-
 	# bind-mount / somewhere else to write below it as well
 	mount --bind "$fsroot" /target || error "Could not bind mount rootfs"
 	mount -o remount,rw /target || error "Could not make rootfs rw"
@@ -26,7 +22,7 @@ overwrite_to_target() {
 		dir="${file%/*}"
 		mkdir_p_target "$dir"
 		rm -rf --one-file-system "${TARGET:-inval}/$f"
-		cp -a "$file" "$TARGET/$file" \
+		cp -a "$fsroot$file" "$TARGET/$file" \
 			|| error "Failed to copy $file from previous rootfs"
 	done
 }
@@ -83,6 +79,10 @@ check_update_log_encryption() {
 }
 
 post_rootfs() {
+	# support older version of overlayfs
+	local fsroot=/live/rootfs/
+	[ -e "$fsroot" ] || fsroot=/
+
 	# Sanity check: refuse to continue if someone tries to write a
 	# rootfs that was corrupted or "too wrong": check for /bin/sh
 	if ! [ -e /target/bin/sh ]; then

commit fd97e668608be8c1705a5b6544d65536020f1676

scripts/pre_rootfs: fix preserve_files copy when file only in overlay
the 'source file must exist' check was being done on overlay path,
but copy itself was made on read-only rootfs so files that were
not preserved but scheduled for copy would fail to update.

Change-Id: I90d57d88affe1b028450c19c6217116ed49f79a5
Fixes: 193d9a81e0ad ("scripts: copy swupdate-preserve-files from rootfs")

scripts/pre_rootfs.sh
@@ -4,7 +4,7 @@ copy_to_target() {
 
 	for file; do
 		# source file must exist... being careful of symlinks
-		[ -L "$file" ] || [ -e "$file" ] || continue
+		[ -L "$fsroot$file" ] || [ -e "$fsroot$file" ] || continue
 		# and destination file not (probably already copied)
 		# except directories which we'll copy into with no-cobbler
 		[ -e "$TARGET/$file" ] && [ ! -d "$TARGET/$file" ] && continue