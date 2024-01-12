atmark-techno/mkswu

commit 840e5a068b5918b4812b2d7ab344ad990e7ee01d

scripts/post_boot: consistently use target's env
Always use new side's environment when using it for sanity checks

Change-Id: Iee7ed0ec37b63cc418d9a2d12012a0629751a52f

scripts/post_boot.sh
@@ -6,11 +6,11 @@ allow_upgrade_available() {
 	[ -z "$encrypted_boot" ] || return
 
 	# Cannot fw_setenv without this...
-	[ -s /etc/fw_env.config ] || return
+	[ -s /target/etc/fw_env.config ] || return
 
 	# Do not set upgrade_available if it is not set in default
 	# configuration.
-	cat /boot/uboot_env.d/* 2>/dev/null | awk -F= '
+	cat /target/boot/uboot_env.d/* 2>/dev/null | awk -F= '
 		$1 == "upgrade_available" {
 			set=$2
 		}
@@ -108,7 +108,7 @@ cleanup_boot() {
 		return
 	fi
 
-	if [ -e /etc/fw_env.config ] \
+	if [ -e /target/etc/fw_env.config ] \
 	    && fw_printenv dek_spl_offset | grep -q dek_spl_offset=0x; then
 		encrypted_boot=1
 	fi
@@ -173,7 +173,7 @@ cleanup_boot() {
 			mmc bootpart enable "$((ab+1))" 0 "$rootdev" \
 				|| error "Could not flip mmc boot flag"
 		fi
-	elif [ -s /etc/fw_env.config ]; then
+	elif [ -s /target/etc/fw_env.config ]; then
 		cleanup_target
 		# if uboot env is supported, use it (e.g. sd card)
 		fw_setenv_nowarn mmcpart $((ab+1)) \

commit 85f939ce4b4b146fb625a86241ebbf68f6e287ec

scripts/post_boot: fix setting upgrade_available on old side
We were incorrectly looking at /target files for this after /target had
been unmounted: use current side's config to set current's side rollback
flag.

Fixes: 840e5a068b59 ("scripts/post_boot: consistently use target's env")
Change-Id: I3d0c7525c179cc0f4d2738493916a803c457bc4f

scripts/post_boot.sh
@@ -6,11 +6,12 @@ allow_upgrade_available() {
 	[ -z "$encrypted_boot" ] || return
 
 	# Cannot fw_setenv without this...
-	[ -s /target/etc/fw_env.config ] || return
+	# Note we're setting current's env after target has been unmounted!
+	[ -s /etc/fw_env.config ] || return
 
 	# Do not set upgrade_available if it is not set in default
 	# configuration.
-	cat /target/boot/uboot_env.d/* 2>/dev/null | awk -F= '
+	cat /boot/uboot_env.d/* 2>/dev/null | awk -F= '
 		$1 == "upgrade_available" {
 			set=$2
 		}