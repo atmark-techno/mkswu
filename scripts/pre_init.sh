atmark-techno/mkswu

commit 7698d77c0ba412ce1b860a4b3be72d8d27b824b3

scripts/SW_ALLOW_ROLLBACK pre check: use sw-versions in /var/log
This check is purely an optimization to avoid endless install loops,
we can immediately start using the sw-versions copy we just created

Change-Id: I1294789a0762a77e48ab718ff535204f3c2e4231

scripts/pre_init.sh
@@ -83,18 +83,13 @@ fail_redundant_update() {
 			error "Nothing to do -- failing on purpose to save bandwidth"
 		fi
 		# also check B-side unless SW_ALLOW_ROLLBACK is set
-		local dev="${partdev}$((ab+1))"
+		local dev="${partdev#/dev/}$((ab+1))"
 		if [ -z "$SW_ALLOW_ROLLBACK" ] \
 		    && [ -n "$upgrade_available" ] \
-		    && luks_unlock "rootfs_$ab" \
-		    && mount -t btrfs,ext4 "$dev" /target 2>/dev/null; then
-			if cmp -s /target/etc/sw-versions \
-					"$SCRIPTSDIR/sw-versions.merged"; then
-				rm -rf "$SCRIPTSDIR"
-				error "Update looks like it already had been installed but rolled back, failing on purpose." \
-					"Set SW_ALLOW_ROLLBACK=1 environment variable to force installing anyway."
-			fi
-			umount /target
+		    && cmp -s "/var/log/swupdate/$dev" "$SCRIPTSDIR/sw-versions.merged"; then
+			rm -rf "$SCRIPTSDIR"
+			error "Update looks like it already had been installed but rolled back, failing on purpose." \
+				"Set SW_ALLOW_ROLLBACK=1 environment variable to force installing anyway."
 		fi
 	fi
 }

commit a12c70531b2fb4673e3981e2b62c0be433f3e841

scripts/pre: fix 'SW_ALLOW_ROLLBACK' check
Changing this optimization to look at /var/log file instead of actually
mounting B-side never worked because it didn't use the correct file
name...

Fixes: 7698d77c0ba4 ("scripts/SW_ALLOW_ROLLBACK pre check: use sw-versions in /var/log")
Change-Id: I0099cbbdc9ef6d41897457612f77fee17a6f1d66

scripts/pre_init.sh
@@ -114,7 +114,7 @@ fail_redundant_update() {
 		local dev="${partdev#/dev/}$((ab+1))"
 		if [ -z "$SW_ALLOW_ROLLBACK" ] \
 		    && [ -n "$upgrade_available" ] \
-		    && cmp -s "/var/log/swupdate/$dev" "$MKSWU_TMP/sw-versions.merged"; then
+		    && cmp -s "/var/log/swupdate/sw-versions-$dev" "$MKSWU_TMP/sw-versions.merged"; then
 			rm -rf "$MKSWU_TMP"
 			error "Update looks like it already had been installed but rolled back, failing on purpose." \
 				"Set SW_ALLOW_ROLLBACK=1 environment variable to force installing anyway."