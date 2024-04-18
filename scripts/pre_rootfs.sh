atmark-techno/mkswu

commit 0da2b0346f56fcc0449b01fd2a36ada241d46fdd

mkswu: add NO_PRESERVE_FILES option
if a user manage their local modifications themselves in their own base_os the
swupdate_preserve_files mechanism is a nuisance and should have an option to
be disabled.
Setting NO_PRESERVE_FILES in desc file or mkswu.conf will do that.

Note that we eventually will want to handle the 'post' phase of
swupdate_preserve_files in a separate step immediately after base_os,
so making it a standalone option is better than having users rely on
DEBUG_SWDESC

scripts/pre_rootfs.sh
@@ -20,6 +20,8 @@ update_preserve_list() {
 	local TARGET="${TARGET:-/target}"
 	local list="$TARGET/etc/swupdate_preserve_files"
 
+	grep -q "NO_PRESERVE_FILES" "$SWDESC" && return
+
 	mkdir_p_target /etc
 
 	if [ -e "/etc/swupdate_preserve_files" ]; then
@@ -103,6 +105,7 @@ copy_preserve_files() {
 	local TARGET="${TARGET:-/target}"
 	local IFS='
 '
+	grep -q "NO_PRESERVE_FILES" "$SWDESC" && return
 
 	grep -E '^/' "$TARGET/etc/swupdate_preserve_files" \
 		| sort -u > "$TMPDIR/preserve_files_pre"

README
@@ -44,6 +44,8 @@ CONTAINER_CLEAR: if set, swupdate will kill running containers and
                  /etc/atmark/containers/*.conf before continuing install.
                  /!\ if install fails this will leave your system
                  with no application, usage is discouraged /!\
+NO_PRESERVE_FILES: if set, swupdate will not copy files listed in
+                   /etc/swupdate_preserve_files when base_os is updated
 
 
 Common options for all helpers:

mkswu
@@ -950,6 +950,7 @@ EOF
 	fi
 	[ -n "$FORCE_VERSION" ] && echo "  #FORCE_VERSION"
 	[ -n "$CONTAINER_CLEAR" ] && echo "  #CONTAINER_CLEAR"
+	[ -n "$NO_PRESERVE_FILES" ] && echo "  #NO_PRESERVE_FILES"
 	case "$POST_ACTION" in
 	poweroff) echo " #POSTACT_POWEROFF";;
 	wait) echo " #POSTACT_WAIT";;

scripts/post_rootfs.sh
@@ -165,6 +165,7 @@ post_copy_preserve_files() {
 	local TARGET="${TARGET:-/target}"
 	local IFS='
 '
+	grep -q "NO_PRESERVE_FILES" "$SWDESC" && return
 
 	sed -ne 's:^POST /:/:p' "$TARGET/etc/swupdate_preserve_files" \
 		| sort -u > "$TMPDIR/preserve_files_post"

commit 9769c3afbf9e77c3caab9cd23831294e5e69df06

scripts/pre_rootfs: fix use with NO_PRESERVE_FILES
When using NO_PRESERVE_FILES /etc is not created and writing update timestamp fails as follow:
[ERROR] : SWUPDATE failed [0] ERROR : /usr/libexec/mkswu/pre.sh: /usr/libexec/mkswu/pre_rootfs.sh: line 351: can't create /target/etc/.rootfs_update_timestamp: nonexistent directory


Fix by always creating /etc

This never worked?!

Link: https://armadillo.atmark-techno.com/forum/armadillo/19879#comment-16106
Fixes: 0da2b0346f56 ("mkswu: add NO_PRESERVE_FILES option")
Change-Id: Id15062898687c59f918e658e0cb203e6e77a0007

scripts/pre_rootfs.sh
@@ -24,8 +24,6 @@ update_preserve_list() {
 
 	[ -n "$(mkswu_var NO_PRESERVE_FILES)" ] && return
 
-	mkdir_p_target /etc
-
 	if [ -e "/etc/swupdate_preserve_files" ]; then
 		cp /etc/swupdate_preserve_files "$list" \
 			|| error "Could not copy swupdate_preserve_files over"
@@ -333,6 +331,7 @@ mount_target_rootfs() {
 	touch /target/.created
 
 	if needs_update "base_os"; then
+		mkdir_p_target /etc
 		info "Updating base os: copying swupdate_preserve_files"
 		update_preserve_list
 		copy_preserve_files