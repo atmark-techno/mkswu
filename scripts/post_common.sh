1dd55093a68ec3cad69b8c8ca2a27d1027930de7
Dominique Martinet <dominique.martinet@atmark-techno.com>
2024-06-03 15:49:40 +0900

    scripts/update_swupdate_certificate: add more error checks & info
    - add errors if rm failed
    - if one-time public certificate was found, output whether it is kept or
    not to info
    
    Change-Id: Ifb9069f9c72502a63438eba36c759b873e0637a4

--- scripts/post_common.sh
+++ scripts/post_common.sh
@@ -49,7 +49,10 @@ update_swupdate_certificate()  {
 		# Armadillo public one-time cert
 		"MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEYTN7NghmISesYQ1dnby5YkocLAe2/EJ8OTXkx/xGhBVlJ57eGOovtPORd/JMkA6lWI0N/pD5p6eUGcwrQvRtsw==")
 			# remove duplicates if it was already set
-			[ -n "$public_onetime_path" ] && rm -f "$public_onetime_path"
+			if [ -n "$public_onetime_path" ]; then
+				rm -f "$public_onetime_path" \
+					|| error "Could not remove temporary file"
+			fi
 			# we don't remove it immediately because we allow updates from atmark
 			# updates to leave it
 			public_onetime_path="$cert"
@@ -59,13 +62,19 @@ update_swupdate_certificate()  {
 		"MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEjgbd3SI8+iof3TLL9qTGNlQN84VqkESPZ3TSUkYUgTiEL3Bi1QoYzGWGqfdmrLiNsgJX4QA3gpaC19Q+fWOkEA=="| \
 		"MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAERkRP5eTXBTG760gEmBfCBz4fWyYfUx3a+sYyHe4uc1sQN2bavxfaBlJmyGI4MY/Pkjh5FDVcddZfil552WUoWQ==")
 			# atmark certificates: delete if we have new ones, or just keep.
-			[ -n "$atmark_present" ] && rm -f "$cert"
+			if [ -n "$atmark_present" ]; then
+				rm -f "$cert" \
+					|| error "Could not remove temporary file"
+			fi
 			atmark_seen=1
 			;;
 		*)
 			# user certificates: delete if we have new ones,
 			# otherwise keep whatever we have here.
-			[ -n "$user_present" ] && rm -f "$cert"
+			if [ -n "$user_present" ]; then
+				rm -f "$cert" \
+					|| error "Could not remove temporary file"
+			fi
 			user_seen=1
 			;;
 		esac
@@ -77,8 +86,11 @@ update_swupdate_certificate()  {
 			# fail unless explicitely allowed
 			[ -n "$(mkswu_var ALLOW_PUBLIC_CERT)" ] \
 				|| error "The public one-time swupdate certificate can only be used once. Please add your own certificate. Failing update."
+			info "No user certificate present: keeping one-time public certificate"
 		else
-			rm -f "$public_onetime_path"
+			rm -f "$public_onetime_path" \
+				|| error "Could not remove temporary file"
+			info "Removing one-time public certificate"
 		fi
 	fi
 	(
