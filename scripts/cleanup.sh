atmark-techno/mkswu

commit 6fe114c784456a5f04ddb66530591e5cf2446a53

scripts/post fail action: run immediately on failure
back when this post hook was implemented we did not have any cleanup
script in place that would get executed immediately by swupdate,
but this is now possible:
- use new cleanup.sh to run the script if swupdate is recent enough
- in this case don't setup the hook at all: less work, less risk!

Change-Id: I7245155e87caf9e0e6163c3197bba988244febdd

scripts/cleanup.sh
@@ -15,6 +15,13 @@ if ! is_locked; then
 	exit 0
 fi
 
+# run post hook if present
+if [ -n "$SWUPDATE_VERSION" ] \
+    && action="$(mkswu_var NOTIFY_FAIL_CMD)" \
+    && [ -n "$action" ]; then
+	eval "$action"
+fi
+
 cleanup
 rm -rf "$MKSWU_TMP"
 unlock_update

scripts/pre_init.sh
@@ -130,6 +130,19 @@ init_fail_command() {
 		rm -f "$fail_file"
 	fi
 
+	# Older swupdate did not provide any generic way of executing a
+	# command after swupdate failure, or when it is over. . .
+	# - for old swupdate, it will remove the scripts dir and we can
+	# watch this here: if $fail_file still exists this was a failure
+	# and we should run it.
+	# - for newer swupdate, cleanup.sh will be run immediately on
+	# failure; so we can skip this.
+	# SWUPDATE_VERSION was first added in 2023.12, so don't bother
+	# checking value.
+	if [ -n "$SWUPDATE_VERSION" ]; then
+		return
+	fi
+
 	action="$(mkswu_var NOTIFY_FAIL_CMD)"
 	[ -z "$action" ] && return
 
@@ -140,11 +153,6 @@ init_fail_command() {
 			&& mv "$tmpfile" "$fail_file"
 	) || error "Could not record NOTIFY_FAIL command"
 
-	# swupdate does not provide any generic way of executing a command
-	# after swupdate failure, or when it is over. . . But we can
-	# rely on the fact that swupdate cleans up the scripts dir when
-	# done, and that we will have removed post_fail_action file on
-	# success, so we wait for that directory to disappear in a subprocess.
 	(
 		# inotifyd exits when it cannot watch anymore, but
 		# we need to chdir out of it first...

commit 0f49fc86c10c98a269df94a900ab9e28d3b51f11

scripts: fail action: fix running fail action on pre-script failure
The fix to make fail action properly run from config on failure did not
properly run it if failure happened during setup (which should never
happen as that means updates are not installable, but can still happen
in theory)

We do not want to run the fail script before updates really started,
e.g. if updates have been rejected because of version check, so:
- remember that update really started in a new MKSWU_TMP file
- stop checking for the old lock, error() would free the lock and make
  cleanup.sh skip this incorrectly
- check the new file instead

also make it so that upgrade_available is cleared first to avoid
rollback; it's unlikely but possible that the start action makes the
target system unreliable so we should not rollback to it before this.

Fixes: 6fe114c78445 ("scripts/post fail action: run immediately on failure")
Change-Id: I48e6d9cd7d25e35a302529cd34c045d2455303c4

scripts/cleanup.sh
@@ -10,13 +10,13 @@ SCRIPTSDIR="$MKSWU_TMP"
 
 . "$SCRIPTSDIR/common.sh"
 
-if ! is_locked; then
-	# already cleaned up (happens if e.g. pre script failed)
-	exit 0
-fi
-
 # run post hook if present
+# (we check SWUPDATE_VERSION here to avoid overlapping with
+#  the async failure mechanism in scripts/pre_init.sh, and
+#  check update_started to avoid running fail script on early
+#  version check failures)
 if [ -n "$SWUPDATE_VERSION" ] \
+    && [ -e "$MKSWU_TMP/update_started" ] \
     && action="$(mkswu_var NOTIFY_FAIL_CMD)" \
     && [ -n "$action" ]; then
 	eval "$action"

scripts/common.sh
@@ -480,7 +480,7 @@ clear_b_side() {
 	# (we could rollback-clone, but that's best left up to users to re-do on
 	# next boot if required)
 
-	# if /target isn't mounted then B side wasn't touched yet
+	# if /target isn't mounted then B side wasn't touched yet (or already done)
 	if ! is_mountpoint /target; then
 		return
 	fi

scripts/pre_init.sh
@@ -168,16 +168,10 @@ init_fail_command() {
 
 init_really_starting() {
 	# if we got here we're really updating:
-	# - handle a fail command if there is one
-	# - signal we're starting an update if instructed
 	# - mark the other partition as unbootable for rollback
-	init_fail_command
-
-	action="$(mkswu_var NOTIFY_STARTING_CMD)"
-	( eval "$action"; ) || error "NOTIFY_STARTING_CMD failed"
-
-	# we won't be able to reboot into other partition until installer
-	# finished; disable rollback
+	# - mark update as started in MKSWU_TMP (for cleanup)
+	# - handle a fail command if there is one (older swupdate only)
+	# - signal we're starting an update if instructed
 	if [ -e "/etc/fw_env.config" ]; then
 		fw_setenv_nowarn upgrade_available
 		case "$?" in
@@ -198,6 +192,12 @@ init_really_starting() {
 			error "Could not set u-boot environment variable, refusing to run"
 		esac
 	fi
+
+	touch "$MKSWU_TMP/update_started"
+	init_fail_command
+
+	action="$(mkswu_var NOTIFY_STARTING_CMD)"
+	( eval "$action"; ) || error "NOTIFY_STARTING_CMD failed"
 }
 
 # when run in installer environment we skip most of the scripts