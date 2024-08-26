atmark-techno/mkswu

commit abcba63b842a46cef7b90e23bdb9208eac1d1860

scripts/pre_rootfs: add mount_rootfs helper
We want to use force-compress for btrfs rootfs mounts, use new helper for this.

We do not need it for the mount in pre_init as that is immediately unmounted,
but the order of mount has been changed to btrfs first as we will start assuming
btrfs by default shortly (it doesn't really matter anyway)

Change-Id: Ib0993aa2d8956ad1922fe30438d264cd412f83f3

scripts/pre_rootfs.sh
@@ -170,6 +170,36 @@ copy_preserve_files() {
 	rm -f "$SCRIPTSDIR/preserve_files_pre"
 }
 
+# mount helper for rootfs
+mount_rootfs_btrfs() {
+        mount -t btrfs -o compress-force=zstd,discard=async,noatime \
+                "$src" "$dst" >/dev/null 2>&1
+}
+
+mount_rootfs_ext4() {
+        mount -t ext4 -o noatime "$src" "$dst" >/dev/null 2>&1
+}
+
+mount_rootfs() {
+        local src="$1" dst="$2" hint_fstype="${3-}"
+
+        [ -n "$hint_fstype" ] \
+		|| hint_fstype="$(findmnt -nr -o FSTYPE /live/rootfs)" \
+                || error "Could not get rootfs fstype"
+
+        case "$hint_fstype" in
+        btrfs)
+                mount_rootfs_btrfs || mount_rootfs_ext4
+                ;;
+        ext4)
+                mount_rootfs_ext4 || mount_rootfs_btrfs
+                ;;
+        *)
+                error "Unsupported filesystem for rootfs: $hint_fstype"
+                ;;
+        esac
+}
+
 mount_target_rootfs() {
 	local dev="${partdev}$((ab+1))"
 	local uptodate basemount
@@ -219,7 +249,7 @@ mount_target_rootfs() {
 	if [ -n "$upgrade_available" ] \
 	    && ! needs_update "base_os" \
 	    && luks_unlock "rootfs_$ab" \
-	    && mount -t ext4,btrfs "$dev" /target 2>/dev/null; then
+	    && mount_rootfs "$dev" /target; then
 		if [ ! -e /target/.created ] \
 		    && [ -s /etc/.rootfs_update_timestamp ] \
 		    && [ "$(cat /etc/.rootfs_update_timestamp 2>/dev/null)" \
@@ -246,16 +276,15 @@ mount_target_rootfs() {
 	btrfs)
 		mkfs.btrfs -q -L "rootfs_${ab}" -m dup -f "$dev" \
 			|| error "Could not reformat $dev"
-		mount -t btrfs "$dev" "/target" -o compress=zstd,discard=async
 		;;
 	ext4)
 		mkfs.ext4 -q ${extlinux:+-O "^64bit"} -L "rootfs_${ab}" -F "$dev" \
 			|| error "Could not reformat $dev"
-		mount -t ext4 "$dev" "/target" || error "Could not mount $dev"
 		;;
 	*)
 		error "Unexpected fstype for rootfs: $fstype. Must be ext4 or btrfs"
 	esac
+	mount_rootfs "$dev" "/target" "$fstype"
 
 	mkdir -p /target/boot /target/mnt /target/target
 	touch /target/.created

scripts/pre_init.sh
@@ -90,7 +90,7 @@ fail_redundant_update() {
 		if [ -z "$SW_ALLOW_ROLLBACK" ] \
 		    && [ -n "$upgrade_available" ] \
 		    && luks_unlock "rootfs_$ab" \
-		    && mount -t ext4,btrfs "$dev" /target 2>/dev/null; then
+		    && mount -t btrfs,ext4 "$dev" /target 2>/dev/null; then
 			if cmp -s /target/etc/sw-versions \
 					"$SCRIPTSDIR/sw-versions.merged"; then
 				rm -rf "$SCRIPTSDIR"

commit 6bab67e52c47cbc9e73f67bf852d92cec60d4ce1

scripts/pre_rootfs: fix mount_rootfs for ABOS 3.14
The very first release of ABOS had the rootfs mounted differently, so
this command fails if the swu is installed on ABOS 3.14

This was not detected by tests because a SWU updating base_os would not
run through this code, and tests only install base_os updates from old
versions -- since no-one has noticed in over a year though we can hope
that everyone running this obsolete version have upgraded by now and
consider dropping support for it soon, but might as well fix this as the
immediate solution is easy enough.

Fixes: abcba63b842a ("scripts/pre_rootfs: add mount_rootfs helper")
Change-Id: Ic7e423558ce88ce4fb7f8a49d132a31ca596ccb6

scripts/pre_rootfs.sh
@@ -217,7 +217,7 @@ mount_rootfs() {
 	local src="$1" dst="$2" hint_fstype="${3-}"
 
 	[ -n "$hint_fstype" ] \
-		|| hint_fstype="$(findmnt -nr -o FSTYPE /live/rootfs)" \
+		|| hint_fstype="$(findmnt -nr -o FSTYPE "${fsroot:-/live/rootfs}")" \
 		|| error "Could not get rootfs fstype"
 
 	case "$hint_fstype" in