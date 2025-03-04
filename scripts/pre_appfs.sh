atmark-techno/mkswu

commit 303f88fbf2be134f03056de541d2591707474bc8

scripts/appfs: use podman stop instead of podman kill
podman kill sends SIGKILL directly, while stops first sends sigterm then
kill after 10s. This leaves some time for containers to handle shutdown
a bit more properly if required.

Lower timeout from 30 to 20s to take these 10s into account.

scripts/pre_appfs.sh
@@ -44,9 +44,9 @@ umount_or_reboot() {
 podman_killall() {
 	if [ -n "$(command podman ps --format '{{.ID}}')" ]; then
 		warning "$@"
-		podman kill -a
+		podman stop -a
 		podman ps --format '{{.ID}}' \
-			| timeout 30s xargs -r podman wait
+			| timeout 20s xargs -r podman wait
 	fi
 	podman pod rm -a -f
 	podman rm -a -f

scripts/post_appfs.sh
@@ -24,9 +24,9 @@ exchange_btrfs_snapshots() {
 podman_killall() {
 	if [ -n "$(command podman ps --format '{{.ID}}')" ]; then
 		warning "$@"
-		podman kill -a
+		podman stop -a
 		podman ps --format '{{.ID}}' \
-			| timeout 30s xargs -r podman wait
+			| timeout 20s xargs -r podman wait
 	fi
 	podman pod rm -a -f
 	podman rm -a -f

commit 8e2164ec4ed1e8db95562cd838f9958b829ec990

scripts/pre_appfs.sh: fix not to use podman stop when killing container
podman_killall() is used to stop containers while executing SWUpdate.
When executing SWUpdate via USB (in unshare -m), and update failed with
CONTAINER_CLEAR due to `podman stop -a` never exiting.

This is because podman stop tries to remove the network namespace
indefinitely from 5.2.1/5.3.0 and thus never terminates.
(Note podman 5.4.0 introduced a 60s timeout here, but this is still much
slower than required)

To resolve this, modify the process to stop containers by sending
sigterm/sigkill directly to the container as appropriate.

Note this will also be resolved when we convert swupdate to run as a
service (even if swupdate-usb runs in a different mount namespace the
actual install process will happen in main namespace), but there is no
downside to using kill; the reason we used stop in the first place was
to send a sigterm first but we can do this manually.

Fixes: 303f88fbf2be ("scripts/appfs: use podman stop instead of podman kill")
Change-Id: I7b24fef1136f70474c5972d9608e353ceb08b30f

scripts/pre_appfs.sh
@@ -100,9 +100,13 @@ remount_or_reboot() {
 podman_killall() {
 	if [ -n "$(podman ps --format '{{.ID}}')" ]; then
 		warning "$@"
-		podman_info stop -a
-		podman ps --format '{{.ID}}' \
-			| timeout 20s xargs -r podman_info wait
+		# give containers a chance to shutdown cleanly, without podman stop
+		# that can hang in a different mount namespace
+		podman_info kill -a -s SIGTERM
+		if ! podman ps --format '{{.ID}}' | timeout 10s xargs -r podman_info wait; then
+			podman_info kill -a -s SIGKILL
+			podman ps --format '{{.ID}}' | timeout 20s xargs -r podman_info wait
+		fi
 		touch "$MKSWU_TMP/podman_containers_killed"
 	fi
 	podman_info pod rm -a -f