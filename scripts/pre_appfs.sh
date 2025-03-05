atmark-techno/mkswu

commit 8e2164ec4ed1e8db95562cd838f9958b829ec990

scripts/pre_appfs.sh: fix not to use podman stop when killing container
podman_killall() is used to stop containers while executing SWUpdate.
When executing SWUpdate via USB (in unshare -m), and update failed with
CONTAINER_CLEAR due to `podman stop -a` never exiting.

This is because podman stop tries to remove the network namespace
indefinitely from 5.2.1/5.3.0 and thus never terminates.
(Note podman 5.4.0 introduced a 60s timeout here, but this is still much
slower than required)

To resolve this, modify the process to stop containers by sending
sigterm/sigkill directly to the container as appropriate.

Note this will also be resolved when we convert swupdate to run as a
service (even if swupdate-usb runs in a different mount namespace the
actual install process will happen in main namespace), but there is no
downside to using kill; the reason we used stop in the first place was
to send a sigterm first but we can do this manually.

Fixes: 303f88fbf2be ("scripts/appfs: use podman stop instead of podman kill")
Change-Id: I7b24fef1136f70474c5972d9608e353ceb08b30f

scripts/pre_appfs.sh
@@ -100,9 +100,13 @@ remount_or_reboot() {
 podman_killall() {
 	if [ -n "$(podman ps --format '{{.ID}}')" ]; then
 		warning "$@"
-		podman_info stop -a
-		podman ps --format '{{.ID}}' \
-			| timeout 20s xargs -r podman_info wait
+		# give containers a chance to shutdown cleanly, without podman stop
+		# that can hang in a different mount namespace
+		podman_info kill -a -s SIGTERM
+		if ! podman ps --format '{{.ID}}' | timeout 10s xargs -r podman_info wait; then
+			podman_info kill -a -s SIGKILL
+			podman ps --format '{{.ID}}' | timeout 20s xargs -r podman_info wait
+		fi
 		touch "$MKSWU_TMP/podman_containers_killed"
 	fi
 	podman_info pod rm -a -f

commit 73460cda6938d133ae49333d77092d1698bda9f8

scripts/app_pre: fix containers termination
After commit 8e2164ec4ed1 ("scripts/pre_appfs.sh: fix not to use podman
stop when killing container"), running swupdate with containers to
terminate prints the following with no waiting time for containers:
[ERROR] : SWUPDATE failed [0] ERROR : xargs: podman_info: No such file or directory

This was actually a problem introduced much before when converting to
the podman_info alias, but podman stop already waited for containers to
be stopped so this was never an actual problem.

- Use the real podman command in xargs
- Redirect stderr to info so the 'wait' "Terminated" message if not exit
  in 10s is printed as info

Fixes: 8e2164ec4ed1 ("scripts/pre_appfs.sh: fix not to use podman stop when killing container")
Fixes: 92141272967d ("podman logs cleanup: rename 'podman' logging alias to podman_info")
Change-Id: I664506e13aeab56dd83e71f811cfd47673d5b216

scripts/pre_appfs.sh
@@ -103,9 +103,11 @@ podman_killall() {
 		# give containers a chance to shutdown cleanly, without podman stop
 		# that can hang in a different mount namespace
 		podman_info kill -a -s SIGTERM
-		if ! podman ps --format '{{.ID}}' | timeout 10s xargs -r podman_info wait; then
+		if ! podman ps --format '{{.ID}}' \
+				| stderr_info timeout 10s xargs -r podman wait; then
 			podman_info kill -a -s SIGKILL
-			podman ps --format '{{.ID}}' | timeout 20s xargs -r podman_info wait
+			podman ps --format '{{.ID}}' \
+				| stderr_info timeout 20s xargs -r podman wait
 		fi
 		touch "$MKSWU_TMP/podman_containers_killed"
 	fi