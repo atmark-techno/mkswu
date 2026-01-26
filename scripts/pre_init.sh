atmark-techno/mkswu

commit bcce8d700b53fe00f9fd8c2dc33a80665e4b8c45

scripts/fail_atmark_new_container: add an escape hatch for atmark updates
fail_atmark_new_container is here to make SWUs like node-red or gw
containers non-installable on armadillo when there already is a
container setup.

This worked fine until now when the only SWUs we want to install on
production machines were OS updates, but it's going to change fast:
- twin agent setting updates
- kernel updates
- firmware updates

The kernel/firmware updates will need to do their own check to avoid
breaking systems, but they will still need to pass this check, so add a
new `swdesc_option ATMARK_PROD_UPDATE` to allow install.

Change-Id: I594b10017f339c2e5ad2b391b1b2be7bdaf6317e

scripts/pre_init.sh
@@ -87,8 +87,13 @@ fail_atmark_new_container() {
 	# we're clearing containers, so nothing to check
 	[ -n "$(mkswu_var CONTAINER_CLEAR)" ] && return
 
-	# base_os update: nothing to check
-	[ -n "$(get_version base_os present)" ] && return
+	if [ -z "$(mkswu_var ATMARK_PROD_UPDATE)" ]; then
+		# [2026/01] temporarily keep for backwards compability with older SWUs:
+		# base_os updates are allowed.
+		if [ -n "$(get_version base_os present)" ]; then
+			return
+		fi
+	fi
 
 	# simple update? check if first version of swu had already been installed
 	read -r component _ < "$MKSWU_TMP/sw-versions.present"

mkswu
@@ -1767,7 +1767,8 @@ swdesc_option() {
 $opt";;
 		CONTAINER_CLEAR|NO_PRESERVE_FILES|\
 		ENCRYPT_ROOTFS|ENCRYPT_USERFS|\
-		SKIP_APP_SUBVOL_SYNC|ALLOW_PUBLIC_CERT|ALLOW_EMPTY_LOGIN)
+		SKIP_APP_SUBVOL_SYNC|ALLOW_PUBLIC_CERT|ALLOW_EMPTY_LOGIN|\
+		ATMARK_PROD_UPDATE)
 			MKSWU_SWDESC_OPTS="$MKSWU_SWDESC_OPTS
 $opt";;
 		*) error "Unknown option for swdesc_option: %s" "$opt";;

commit 18607d3c16d3028a7e036ccad9e2a56eadb5aa4e

scripts/fail_atmark_new_container: fix ATMARK_PROD_UPDATE check
We want to accept updates that have ATMARK_PROD_UPDATE, not to continue
into the containers existence check...

Fixes: bcce8d700b53 ("scripts/fail_atmark_new_container: add an escape hatch for atmark updates")
Change-Id: I178768cbce12bbdea66827715fe26a150ce7f79a

scripts/pre_init.sh
@@ -87,13 +87,12 @@ fail_atmark_new_container() {
 	# we're clearing containers, so nothing to check
 	[ -n "$(mkswu_var CONTAINER_CLEAR)" ] && return
 
-	if [ -z "$(mkswu_var ATMARK_PROD_UPDATE)" ]; then
-		# [2026/01] temporarily keep for backwards compability with older SWUs:
-		# base_os updates are allowed.
-		if [ -n "$(get_version base_os present)" ]; then
-			return
-		fi
-	fi
+	# atmark-vetted update: safe to install even if user applications are setup
+	[ -n "$(mkswu_var ATMARK_PROD_UPDATE)" ] && return
+
+	# [2026/01] temporarily keep for backwards compability with older SWUs:
+	# base_os updates are allowed.
+	[ -n "$(get_version base_os present)" ] && return
 
 	# simple update? check if first version of swu had already been installed
 	read -r component _ < "$MKSWU_TMP/sw-versions.present"