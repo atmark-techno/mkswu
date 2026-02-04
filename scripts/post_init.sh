atmark-techno/mkswu

commit 107edafa913096445fed8f1cf8b7b8b1bdfe8575

scripts/post: split shadows update and passwordless check
- we only want to copy old side's users on base os update; not copying
them for extra_os updates allows commands like userdel to work
- we still need to check for empty passwords: split this command and use
that for installer and extra os updates.

Link: https://armadillo.atmark-techno.com/forum/armadillo/20085
Change-Id: I3fef3891f4b02ca6549646478accab0bbc5a6ea4

scripts/post_init.sh
@@ -5,8 +5,8 @@ post_installer() {
 	# for update_shadow compat, always mounted in installer
 	local fsroot=/live/rootfs
 
-	# update_shadow checks user password has been set unless made optional
-	update_shadow
+	# check passwords have been set if required
+	check_shadow_empty_password
 	# update swupdate certificate and versions
 	update_swupdate_certificate
 	cp "$MKSWU_TMP/sw-versions.merged" "/target/etc/sw-versions" \

scripts/post_common.sh
@@ -1,80 +1,14 @@
 # SPDX-License-Identifier: MIT
 
-update_shadow_user() {
-	local user="$1"
-	local oldpass
-
-	oldpass=$(awk -F':' '$1 == "'"$user"'" && $3 != "0"' "$SHADOW" \
-			| sed -e 's/[\\:&]/\\&/g')
-	# password was never set: skip
-	[ -n "$oldpass" ] || return
-	# password already set on target: also skip
-	grep -qE "^$user:[^!:]" "$NSHADOW" && return
-
-	if grep -qE "^$user:" "$NSHADOW"; then
-		sed -i -e 's:^'"$user"'\:.*:'"$oldpass"':' "$NSHADOW"
-	else
-		echo "$oldpass" | sed -e 's/\\\([\\:&]\)/\1/g' >> "$NSHADOW"
-	fi || error "Could not update shadow for $user"
-}
-
-update_user_groups() {
-	local user="$1"
-	local group
-
-	awk -F: '$4 ~ /(,|^)'"$user"'(,|$)/ { print $1 }' < "$GROUP" |
-		while read -r group; do
-			# already set
-			grep -qE "^$group:.*[:,]$user(,|$)" "$NGROUP" \
-				&& continue
-
-			if grep -qE "^$group:.*:$" "$NGROUP"; then
-				sed -i -e 's/^'"$group"':.*:/&'"$user"'/' "$NGROUP"
-			else
-				sed -i -e 's/^'"$group"':.*/&,'"$user"'/' "$NGROUP"
-			fi || error "Could not update group $group / $user"
-		done
-}
-
-update_shadow() {
-	local user group
-	# test constants
-	local PASSWD="${PASSWD:-$fsroot/etc/passwd}"
-	local NPASSWD="${NPASSWD:-/target/etc/passwd}"
-	local SHADOW="${SHADOW:-$fsroot/etc/shadow}"
+check_shadow_empty_password() {
+	local user
 	local NSHADOW="${NSHADOW:-/target/etc/shadow}"
-	local GROUP="${GROUP:-$fsroot/etc/group}"
-	local NGROUP="${NGROUP:-/target/etc/group}"
-
-	# "$PASSWD", group and shadow have to be part of rootfs as
-	# rootfs updates can change system users, but we want to keep
-	# "real" users as well so copy them over manually:
-	# - copy non-existing "real" groups (gid >= 1000)
-	# - for each real user (root, abos* or uid >= 1000), copy its password
-	# if the old one not set and add it to groups it had been added to
-	awk -F: '$3 >= 1000 && $3 < 65500 { print $1 }' < "$GROUP" |
-		while read -r group; do
-			grep -qE "^$group:" "$NGROUP" \
-				|| grep -E "^$group:" "$GROUP" >> "$NGROUP"
-		done
-
-	awk -F: '$1 == "root" || $1 ~ /^abos/ \
-			|| ( $3 >= 1000 && $3 < 65500 ) {
-				print $1
-			}' < "$PASSWD" |
-		while read -r user; do
-			grep -qE "^$user:" "$NPASSWD" \
-				|| grep -E "^$user:" "$PASSWD" >> "$NPASSWD"
-			update_shadow_user "$user"
-			update_user_groups "$user"
-		done
 
 	# check there are no user with empty login
 	# unless the update explicitely allows it
 	[ -n "$(mkswu_var ALLOW_EMPTY_LOGIN)" ] && return
 	user=$(awk -F: '$2 == "" { print $1 } ' "$NSHADOW")
 	[ -z "$user" ] || error "the following users have an empty password, failing update: $user"
-
 }
 
 

scripts/post_rootfs.sh
@@ -1,3 +1,73 @@
+update_shadow_user() {
+	local user="$1"
+	local oldpass
+
+	oldpass=$(awk -F':' '$1 == "'"$user"'" && $3 != "0"' "$SHADOW" \
+			| sed -e 's/[\\:&]/\\&/g')
+	# password was never set: skip
+	[ -n "$oldpass" ] || return
+	# password already set on target: also skip
+	grep -qE "^$user:[^!:]" "$NSHADOW" && return
+
+	if grep -qE "^$user:" "$NSHADOW"; then
+		sed -i -e 's:^'"$user"'\:.*:'"$oldpass"':' "$NSHADOW"
+	else
+		echo "$oldpass" | sed -e 's/\\\([\\:&]\)/\1/g' >> "$NSHADOW"
+	fi || error "Could not update shadow for $user"
+}
+
+update_user_groups() {
+	local user="$1"
+	local group
+
+	awk -F: '$4 ~ /(,|^)'"$user"'(,|$)/ { print $1 }' < "$GROUP" |
+		while read -r group; do
+			# already set
+			grep -qE "^$group:.*[:,]$user(,|$)" "$NGROUP" \
+				&& continue
+
+			if grep -qE "^$group:.*:$" "$NGROUP"; then
+				sed -i -e 's/^'"$group"':.*:/&'"$user"'/' "$NGROUP"
+			else
+				sed -i -e 's/^'"$group"':.*/&,'"$user"'/' "$NGROUP"
+			fi || error "Could not update group $group / $user"
+		done
+}
+
+update_shadow() {
+	local user group
+	# test constants
+	local PASSWD="${PASSWD:-$fsroot/etc/passwd}"
+	local NPASSWD="${NPASSWD:-/target/etc/passwd}"
+	local SHADOW="${SHADOW:-$fsroot/etc/shadow}"
+	local NSHADOW="${NSHADOW:-/target/etc/shadow}"
+	local GROUP="${GROUP:-$fsroot/etc/group}"
+	local NGROUP="${NGROUP:-/target/etc/group}"
+
+	# "$PASSWD", group and shadow have to be part of rootfs as
+	# rootfs updates can change system users, but we want to keep
+	# "real" users as well so copy them over manually:
+	# - copy non-existing "real" groups (gid >= 1000)
+	# - for each real user (root, abos* or uid >= 1000), copy its password
+	# if the old one not set and add it to groups it had been added to
+	awk -F: '$3 >= 1000 && $3 < 65500 { print $1 }' < "$GROUP" |
+		while read -r group; do
+			grep -qE "^$group:" "$NGROUP" \
+				|| grep -E "^$group:" "$GROUP" >> "$NGROUP"
+		done
+
+	awk -F: '$1 == "root" || $1 ~ /^abos/ \
+			|| ( $3 >= 1000 && $3 < 65500 ) {
+				print $1
+			}' < "$PASSWD" |
+		while read -r user; do
+			grep -qE "^$user:" "$NPASSWD" \
+				|| grep -E "^$user:" "$PASSWD" >> "$NPASSWD"
+			update_shadow_user "$user"
+			update_user_groups "$user"
+		done
+}
+
 update_running_versions() {
 	cp "$1" /etc/sw-versions || error "Could not update /etc/sw-versions"
 
@@ -166,20 +236,23 @@ post_rootfs() {
 	fi
 
 	# extra fixups on update
-	# in theory we should also check shadow/cert if no update, but the system
-	# needs extra os update to start containers so this is enough for safety check
-	if update_rootfs; then
-		# keep passwords around, and make sure there are no open access user left
+	if update_baseos; then
+		# keep passwords around
 		update_shadow
 
-		# remove open access swupdate certificate or complain
-		update_swupdate_certificate
-	fi
-
-	if update_baseos; then
 		# update preserve_files owners when required
 		# (after update_shadow)
 		post_chown_preserve_files
+	fi
+
+	# in theory we should also check shadow/cert if no update, but the system
+	# needs extra os update to start containers so this is enough for safety check
+	if update_rootfs; then
+		# check there are no empty passwords left behind
+		check_shadow_empty_password
+
+		# remove open access swupdate certificate or complain
+		update_swupdate_certificate
 	fi
 
 	# mark filesystem as ready for reuse if something failed

tests/scripts.sh
@@ -326,7 +326,7 @@ test_passwd_update() {
 		cp ./scripts/$f "$MKSWU_TMP/$f-target"
 	done
 	SHADOW=./scripts/shadow
-	( update_shadow; ) && error "copy should have failed"
+	( update_shadow; check_shadow_empty_password; ) && error "copy should have failed"
 
 
 	echo "passwd copy: test adding new user"
@@ -360,7 +360,7 @@ test_passwd_update() {
 	done
 	SHADOW=./scripts/shadow
 	echo "  # MKSWU_ALLOW_EMPTY_LOGIN 1" > "$MKSWU_TMP/swdesc"
-	( SWDESC="$MKSWU_TMP/swdesc" update_shadow; ) \
+	( SWDESC="$MKSWU_TMP/swdesc" check_shadow_empty_password; ) \
 		|| error "should be no failure with allow empty login"
 }
 
@@ -846,9 +846,9 @@ test_update_overlays() {
 	export TEST_SCRIPTS=1
 	cleanup() { :; }
 	. "$SCRIPTS_SRC_DIR/post_common.sh"
-	test_passwd_update
 	test_cert_update
 	. "$SCRIPTS_SRC_DIR/post_rootfs.sh"
+	test_passwd_update
 	test_preserve_files_chown
 ) || error "post test failed"
 

commit 555412ce5aa73bbad079bba0a17c0e85e3873ad7

scripts/post_init: don't set fsroot in post_installer
This is no longer needed since post_installer no longer updates shadows from current
fsroot, and only checks /target

Fixes: 107edafa9130 ("scripts/post: split shadows update and passwordless check")
Change-Id: I7f3dbbae200bfd38dd4f7beba9c79d7be12f8942

scripts/post_init.sh
@@ -2,9 +2,6 @@
 
 # Do minimal work if called from installer
 post_installer() {
-	# for update_shadow compat, always mounted in installer
-	local fsroot=/live/rootfs
-
 	# check passwords have been set if required
 	check_shadow_empty_password
 	# update swupdate certificate and versions