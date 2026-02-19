atmark-techno/mkswu

commit 8fa267af487a37081de67d29b38ff7a34bd6745a

scripts: move normal mkswu scripts dir to scripts-mkswu
swupdate itself removes $TMPDIR/scripts at the end of update, but for
chained updates we will not want $MKSWU_TMP to be removed, so move the
scripts to another directory.

Also update relevant scripts (install_boot_linux/post_rootfs_baseos.sh)
to find rootdev and other state properly

Change-Id: I281c3963eec6330117fe7cdb7531cb936a56d457

scripts/post_common.sh
@@ -21,7 +21,7 @@ update_swupdate_certificate()  {
 	local SWUPDATE_PEM=${SWUPDATE_PEM:-/target/etc/swupdate.pem}
 	# Use tmpdir from SWU version of the scripts as embedded tmp
 	# never contains certificates
-	local MKSWU_SWU_TMP="$TMPDIR/scripts"
+	local MKSWU_SWU_TMP="$TMPDIR/scripts-mkswu"
 
 	# what certificates were embedded into swu, if any?
 	for cert in "$MKSWU_SWU_TMP/certs_atmark/"*; do

Makefile
@@ -146,7 +146,7 @@ install_swupdate:
 	sed -i -e 's/DEBUG_SKIP_SCRIPTS/DEBUG_SKIP_VENDORED_SCRIPTS/' $(DESTDIR)$(LIBEXEC)/common.sh
 	# modify scripts base directory
 	sed -i -e 's:^SCRIPTSDIR=.*:SCRIPTSDIR=$(LIBEXEC):' \
-		-e 's/^MKSWU_TMP=.*/&-vendored/' \
+		-e 's/^\(MKSWU_TMP=.*\)-mkswu/\1-vendored/' \
 		$(DESTDIR)$(LIBEXEC)/pre.sh \
 		$(DESTDIR)$(LIBEXEC)/post.sh \
 		$(DESTDIR)$(LIBEXEC)/cleanup.sh

mkswu
@@ -1179,7 +1179,7 @@ swdesc_boot_linux() {
 
 	eval "$MKSWU_export_swdesc_cmd_vars_stanza"
 	swdesc_exec_nochroot "$image" \
-		'${TMPDIR:-/var/tmp}/scripts/install_boot_linux $1'
+		'${TMPDIR:-/var/tmp}/scripts-mkswu/install_boot_linux $1'
 }
 
 swdesc_tar() {
@@ -1238,7 +1238,7 @@ swdesc_tar() {
 		MKSWU_component_baseos_seen="done"
 		MKSWU_hide_from_show=1 \
 			swdesc_command_nochroot --description "base_os post fixups" \
-				'${TMPDIR:-/var/tmp}/scripts/post_rootfs_baseos.sh'
+				'${TMPDIR:-/var/tmp}/scripts-mkswu/post_rootfs_baseos.sh'
 	fi
 }
 
@@ -1422,7 +1422,7 @@ swdesc_exec() {
 	*,base_os|*,extra_os*)
 		# run in container, writable OS
 		# use podman_target to use newly installed images
-		chroot_cmd='${TMPDIR:-/var/tmp}/scripts/podman_target '"$chroot_cmd"
+		chroot_cmd='${TMPDIR:-/var/tmp}/scripts-mkswu/podman_target '"$chroot_cmd"
 		chroot_cmd="$chroot_cmd --root /target/var/lib/containers"
 		chroot_cmd="$chroot_cmd -v /target:/target $container"
 		;;
@@ -1435,7 +1435,7 @@ swdesc_exec() {
 		;;
 	*)
 		# run in container, read-only OS, RW volumes
-		chroot_cmd='${TMPDIR:-/var/tmp}/scripts/podman_target '"$chroot_cmd"
+		chroot_cmd='${TMPDIR:-/var/tmp}/scripts-mkswu/podman_target '"$chroot_cmd"
 		chroot_cmd="$chroot_cmd -v /target/var/app/volumes:/target/var/app/volumes"
 		chroot_cmd="$chroot_cmd -v /target/var/app/rollback/volumes:/target/var/app/rollback/volumes"
 		chroot_cmd="$chroot_cmd -v /target:/target:ro $container"
@@ -1534,7 +1534,7 @@ swdesc_embed_container() {
 	container_check_archive
 
 	eval "$MKSWU_export_swdesc_cmd_vars_stanza"
-	swdesc_exec_nochroot "$image" '${TMPDIR:-/var/tmp}/scripts/podman_target load $1'
+	swdesc_exec_nochroot "$image" '${TMPDIR:-/var/tmp}/scripts-mkswu/podman_target load $1'
 }
 
 swdesc_pull_container() {
@@ -1543,7 +1543,7 @@ swdesc_pull_container() {
 
 	parse_swdesc pull_container "$@"
 
-	pull_cmd='${TMPDIR:-/var/tmp}/scripts/podman_target pull '
+	pull_cmd='${TMPDIR:-/var/tmp}/scripts-mkswu/podman_target pull '
 	pull_cmd="$pull_cmd ${container_tag:+"--tag \"$container_tag\" "} \"$image\""
 
 	eval "$MKSWU_export_swdesc_cmd_vars_stanza"
@@ -1568,7 +1568,7 @@ swdesc_usb_container() {
 	COPY_USB="$COPY_USB $(shell_quote "$(realpath "$OUTDIR/$image_usb.sig")")"
 
 	eval "$MKSWU_export_swdesc_cmd_vars_stanza"
-	swdesc_command_nochroot '${TMPDIR:-/var/tmp}/scripts/podman_target --pubkey /etc/swupdate.pem load '"$image_usb"
+	swdesc_command_nochroot '${TMPDIR:-/var/tmp}/scripts-mkswu/podman_target --pubkey /etc/swupdate.pem load '"$image_usb"
 }
 
 swdesc_sbom_source_file() {

scripts/cleanup.sh
@@ -4,7 +4,7 @@
 [ -n "$MKSWU_SKIP_SCRIPTS" ] && exit 0
 
 TMPDIR="${TMPDIR:-/var/tmp}"
-MKSWU_TMP="$TMPDIR/scripts"
+MKSWU_TMP="$TMPDIR/scripts-mkswu"
 # SCRIPTSDIR is overridden for scripts embedded with swupdate
 SCRIPTSDIR="$MKSWU_TMP"
 
@@ -23,9 +23,7 @@ if [ -n "$SWUPDATE_VERSION" ] \
 fi
 
 cleanup
-if [ "$MKSWU_TMP" != "$TMPDIR/scripts" ]; then
-	# swupdate removes TMPDIR/scripts itself, but we still
-	# need to remove the -vendored dir if it was used...
-	rm -rf "$MKSWU_TMP"
-fi
+# swupdate removes TMPDIR/scripts itself, but we still
+# need to remove mkswu's dir
+rm -rf "$MKSWU_TMP"
 unlock_update

scripts/common.sh
@@ -40,6 +40,7 @@ error() {
 	fi
 
 	cleanup ""
+	rm -rf "$MKSWU_TMP"
 	unlock_update
 	exit 1
 }

scripts/install_boot_linux
@@ -57,8 +57,8 @@ write_split_boot_partition() {
 }
 
 # state variable from mkswu scripts
-if [ -e "$TMPDIR/scripts/rootdev" ]; then
-	MKSWU_TMP="$TMPDIR/scripts"
+if [ -e "$TMPDIR/scripts-mkswu/rootdev" ]; then
+	MKSWU_TMP="$TMPDIR/scripts-mkswu"
 elif [ -e "$TMPDIR/scripts-vendored/rootdev" ]; then
 	MKSWU_TMP="$TMPDIR/scripts-vendored"
 else

scripts/podman_cleanup
@@ -181,8 +181,8 @@ podman_prune() {
 cleanup_images() {
 	local tmpdir
 
-	mkdir -p "${TMPDIR:-/var/tmp}/scripts"
-	tmpdir=$(mktemp -d "${TMPDIR:-/var/tmp}/scripts/mkswu_podman_cleanup.XXXXXX") \
+	mkdir -p "${TMPDIR:-/var/tmp}/scripts-mkswu"
+	tmpdir=$(mktemp -d "${TMPDIR:-/var/tmp}/scripts-mkswu/mkswu_podman_cleanup.XXXXXX") \
 		|| error "Could not create tmpdir in ${TMPDIR:-/var/tmp}"
 	trap "rm -rf '$tmpdir'" EXIT
 

scripts/podman_target
@@ -203,8 +203,8 @@ update_containers() {
 
 	# podman sometimes leaves lots of data in /var/tmp on errors:
 	# force a different TMPDIR and clean up after podman
-	mkdir -p "${TMPDIR:-/var/tmp}/scripts"
-	tmp=$(mktemp -d "${TMPDIR:-/var/tmp}/scripts/mkswu_podman_target.XXXXXX") \
+	mkdir -p "${TMPDIR:-/var/tmp}/scripts-mkswu"
+	tmp=$(mktemp -d "${TMPDIR:-/var/tmp}/scripts-mkswu/mkswu_podman_target.XXXXXX") \
 		|| error "Could not create tmpdir in ${TMPDIR:-/var/tmp}"
 	export TMPDIR="$tmp"
 	trap cleanup EXIT INT QUIT TERM

commit 5526f3d2c1d0377b202f08cf1ade410c21305005

scripts/update_swupdate_certificate: fix vendored update of older SWUs
older SWUs will extract certs in $TMPDIR/scripts, so we need to check
both directories for the forseeable future.

Fixes: 8fa267af487a ("scripts: move normal mkswu scripts dir to scripts-mkswu")
Change-Id: Ief4061d9f938ae1b14e93d63803c6c20f3289fa8

scripts/post_common.sh
@@ -18,10 +18,13 @@ update_swupdate_certificate()  {
 	local atmark_present="" atmark_seen="" atmark_old_seen=""
 	local user_present="" user_seen=""
 	# test constants
-	local SWUPDATE_PEM=${SWUPDATE_PEM:-/target/etc/swupdate.pem}
+	local SWUPDATE_PEM="${SWUPDATE_PEM:-/target/etc/swupdate.pem}"
 	# Use tmpdir from SWU version of the scripts as embedded tmp
 	# never contains certificates
+	# 2026/02: also check older directory for older SWUs
+	# This will be removed in a couple of years...
 	local MKSWU_SWU_TMP="$TMPDIR/scripts-mkswu"
+	local OLD_MKSWU_SWU_TMP="$TMPDIR/scripts"
 
 	# what certificates were embedded into swu, if any?
 	for cert in "$MKSWU_SWU_TMP/certs_atmark/"*; do
@@ -129,12 +132,14 @@ update_swupdate_certificate()  {
 		cat "$certsdir"/cert.* 2>/dev/null
 		if [ -n "$atmark_seen" ]; then
 			# only add atmark certs if they're currently installed
-			for cert in "$MKSWU_SWU_TMP/certs_atmark/"*; do
+			for cert in "$MKSWU_SWU_TMP/certs_atmark/"* \
+					"$OLD_MKSWU_SWU_TMP/certs_atmark/"*; do
 				[ -e "$cert" ] || continue
 				cat "$cert" || exit 1
 			done
 		fi
-		for cert in "$MKSWU_SWU_TMP/certs_user/"*; do
+		for cert in "$MKSWU_SWU_TMP/certs_user/"* \
+				"$OLD_MKSWU_SWU_TMP/certs_user/"*; do
 			[ -e "$cert" ] || continue
 			# add comment to older certificates
 			grep -qE '^# ' "$cert" || echo "# ${cert##*/}"