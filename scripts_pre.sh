atmark-techno/mkswu

commit 1545d31de1117f10d02d3f79ebfe73b400dd5f5b

scripts: implement chained SWU support
This allows installing multiple SWUs without rebooting in between, with
a few limitations:
- If any SWU in the chain contains base_os or boot, they must be
installed first. Trying to install one in the middle will stop the
installation where it is and reboot, restarting the chain will skip
after reboot will skip all the earlier SWUs and start with that update
successfully
- Likewise, any special variables handled in mkswu pre/post steps (like
CONTAINER_CLEAR) won't work -- but these are not checked.

A script to install multiple SWUs from local files or network will be
provided in abos-base.

Change-Id: I086186257b25d53c23751cb7cc42c2a3ddf10b18

scripts_pre.sh
@@ -11,8 +11,26 @@ MKSWU_TMP="$TMPDIR/scripts-mkswu"
 # SCRIPTSDIR is overridden for scripts embedded with swupdate
 SCRIPTSDIR="$MKSWU_TMP"
 
-rm -rf "$MKSWU_TMP"
-mkdir "$MKSWU_TMP" || exit 1
+if [ "${SWUPDATE_CHAIN_IDX:-1}" = 1 ]; then
+	# only re-create dir for first swu in chain
+	rm -rf "$MKSWU_TMP"
+	mkdir "$MKSWU_TMP" || exit 1
+	# 2026/02: also remove old scripts dir for leftovers of older SWUs
+	rm -rf "$TMPDIR/scripts"
+else
+	if ! [ -e "$MKSWU_TMP" ]; then
+		echo "Chained SWU but $MKSWU_TMP does not exist!" >&2
+		exit 1
+	fi
+	# cleanup any certs and re-extract (they are installed
+	# after each individual swu if present)
+	# Note scripts-mkswu is hardcoded as we only consider the
+	# SWU tarball.
+	# 2026/02: also remove old scripts dir for leftovers of older SWUs
+	rm -rf "$TMPDIR/scripts-mkswu/certs_"* \
+		"$TMPDIR/scripts/certs_"*
+fi
+
 cd "$MKSWU_TMP" || exit 1
 
 # extract archive after script

scripts/cleanup.sh
@@ -13,7 +13,30 @@ fi
 
 . "$SCRIPTSDIR/common.sh"
 
+handle_chained_swu() {
+	# Updates with nothing to do are considered failures by swupdate,
+	# but we don't want this script to run for chained updates in this
+	# case, so check manually...
+
+	# We still want to cleanup $TMPDIR/scripts-vendored on nothing
+	# to do if this was not a chained update, so we only check this on chained update
+	[ -n "$SWUPDATE_CHAIN_IDX" ] || return
+
+	# Not nothing to do -> real error, cleanup normally
+	[ -e "$MKSWU_TMP/nothing_to_do" ] || return
+
+	# not last in chain, don't cleanup.
+	[ "$SWUPDATE_CHAIN_IDX" != "$SWUPDATE_CHAIN_COUNT" ] && exit
+
+	# last in chain, run post to finish update.
+	# (but still need cleanup if that failed...
+	# hence subshell to catch if it exists)
+	( "$SCRIPTSDIR/post.sh"; ) && exit 0
+}
+
 do_cleanup() {
+	handle_chained_swu
+
 	# run post hook if present
 	# (we check SWUPDATE_VERSION here to avoid overlapping with
 	#  the async failure mechanism in scripts/pre_init.sh, and

scripts/common.sh
@@ -24,6 +24,7 @@ error() {
 
 	if [ -n "$NOTHING_TO_DO" ]; then
 		log_status "NOTHING_TO_DO"
+		touch "$MKSWU_TMP/nothing_to_do"
 	else
 		log_status "ERROR: $*"
 	fi
@@ -502,6 +503,13 @@ set_post_action() {
 	if [ "$post_action" = "container" ] && needs_reboot; then
 		post_action=""
 	fi
+	# for chained updates the last SWU to be installed can
+	# pick poweroff or wait without problem, but container
+	# would fail if it doesn't agree with the first SWU:
+	# always clear it for now
+	if [ "$post_action" = "container" ] && [ -n "$SWUPDATE_CHAIN_IDX" ]; then
+		post_action=""
+	fi
 }
 
 clear_b_side() {
@@ -601,6 +609,7 @@ clear_internal_variables() {
 	#     SW_ALLOW_ROLLBACK SWUPDATE_FROM_INSTALLER
 	#     SWUPDATE_HAWKBIT SWUPDATE_USB_SWU SWUPDATE_ARMADILLO_TWIN
 	#     CONTAINERS_CONF/CONTAINERS_STORAGE_CONF/CONTAINERS_REGISTRIES_CONF
+	#     SWUPDATE_CHAIN_IDX SWUPDATE_CHAIN_COUNT SWUPDATE_CHAIN_ID
 	# these come from swupdate
 	#     SWUDPATE_WARN_FD SWUPDATE_INFO_FD
 }

scripts/post_init.sh
@@ -1,19 +1,26 @@
 # SPDX-License-Identifier: MIT
 
-# Do minimal work if called from installer
-post_installer() {
+# Do minimal work if skipping cleanup (installer or chained updates)
+post_minimal() {
+	info "$1"
+
 	# check passwords have been set if required
 	check_shadow_empty_password
 	# update swupdate certificate and versions
 	update_swupdate_certificate
 	cp "$MKSWU_TMP/sw-versions.merged" "/target/etc/sw-versions" \
 		|| error "Could not set sw-versions"
+
+	log_status "SUCCESS"
 }
 
 init() {
 	if [ -n "$SWUPDATE_FROM_INSTALLER" ]; then
-		info "SWU post install in installer"
-		post_installer
+		post_minimal "SWU post install in installer"
+		exit 0
+	fi
+	if [ "$SWUPDATE_CHAIN_IDX" != "$SWUPDATE_CHAIN_COUNT" ]; then
+		post_minimal "Skipping post ($SWUPDATE_CHAIN_IDX / $SWUPDATE_CHAIN_COUNT)"
 		exit 0
 	fi
 

scripts/pre_init.sh
@@ -55,6 +55,13 @@ init_vars_update() {
 	    && fw_printenv upgrade_available | grep -qxE 'upgrade_available=[012]'; then
 		upgrade_available=1
 	fi
+	if [ -n "$SWUPDATE_CHAIN_IDX" ]; then
+		# for now consider chained updates to always update rootfs and require
+		# reboot (needs_reboot is implied by the override in set_post_action)
+		# If this changes in the future, pre_chained_update will need to
+		# update saved vars if a later update starts modifying these
+		update_rootfs=${update_rootfs:-1}
+	fi
 }
 
 save_vars() {
@@ -216,6 +223,51 @@ pre_installer() {
 	needs_update boot && error "Cannot update boot image in installer"
 }
 
+# for chained updates after the first one
+pre_chained_update() {
+	info "Processing chain $SWUPDATE_CHAIN_ID update $SWUPDATE_CHAIN_IDX / $SWUPDATE_CHAIN_COUNT"
+
+	if [ "$SWUPDATE_CHAIN_IDX" = 1 ]; then
+		echo "$SWUPDATE_CHAIN_ID" > "$MKSWU_TMP/swupdate_chain_id"
+		# first update setups normally
+		return
+	else
+		local old_id
+
+		old_id=$(cat "$MKSWU_TMP/swupdate_chain_id")
+		[ "$old_id" = "$SWUPDATE_CHAIN_ID" ] \
+			|| error "SWU chain id mismatch (expected $old_id, got $SWUPDATE_CHAIN_ID)"
+	fi
+
+	# if first update had nothing to do, init won't be done yet: continue normally
+	if ! [ -e "$MKSWU_TMP/update_started" ]; then
+		return
+	fi
+
+	mountpoint -q /target \
+		|| error "SWU chain but /target is not mounted"
+
+	# sanity check: /target/etc/sw-versions must be the same as /etc/sw-versions
+	# or swupdate will not install the correct software
+	cmp -s /etc/sw-versions /target/etc/sw-versions \
+		|| error "/etc/sw-versions does not match target's"
+
+	gen_newversion
+	if needs_update base_os || needs_update boot; then
+		# we can't handle base_os or boot update mid-chain,
+		# finish what we have now and this will be installed
+		# after reboot
+		info "base_os or boot update required mid SWU chain. Stopping chain here to continue after reboot"
+		# This needs some fiddling to avoid bumping versions
+		# with what we just get and post to not skip work...
+		cp "$MKSWU_TMP/sw-versions.old" "$MKSWU_TMP/sw-versions.merged" \
+			|| error "Failed cleanup"
+		unset SWUPDATE_CHAIN_IDX SWUPDATE_CHAIN_COUNT
+		exec "$SCRIPTSDIR/post.sh"
+		exit 1
+	fi
+}
+
 check_until() {
 	local until_start until_end now
 
@@ -239,16 +291,20 @@ init() {
 	lock_update
 
 	check_until
+	init_vars
 
 	if [ -n "$SWUPDATE_FROM_INSTALLER" ]; then
 		info "SWU pre install in installer"
 		pre_installer
 		exit 0
 	fi
+	if [ -n "$SWUPDATE_CHAIN_IDX" ]; then
+		# this exits early if further setup is not needed
+		pre_chained_update
+	fi
 
 	cleanup
 
-	init_vars
 	gen_newversion
 	init_vars_update
 

commit 3c2bb9e01e728744b20725597f592695a8a74239

scripts/pre: re-create TMPDIR/scripts after deletion
completely removing dir and not recreating it makes swupdate print an
error on its own cleanup, so recreate it

Reported-by: Satoshi OHTA <satoshi.ohta@atmark-techno.com>
Fixes: 1545d31de111 ("scripts: implement chained SWU support")
Change-Id: I0b8295cbc8521c23bc84ab6b150627c5deab7127

scripts_pre.sh
@@ -16,7 +16,9 @@ if [ "${SWUPDATE_CHAIN_IDX:-1}" = 1 ]; then
 	rm -rf "$MKSWU_TMP"
 	mkdir "$MKSWU_TMP" || exit 1
 	# 2026/02: also remove old scripts dir for leftovers of older SWUs
+	# (prints error message if missing so recreate)
 	rm -rf "$TMPDIR/scripts"
+	mkdir -p "$TMPDIR/scripts"
 else
 	if ! [ -e "$MKSWU_TMP" ]; then
 		echo "Chained SWU but $MKSWU_TMP does not exist!" >&2