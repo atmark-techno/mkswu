atmark-techno/mkswu

commit 8fa267af487a37081de67d29b38ff7a34bd6745a

scripts: move normal mkswu scripts dir to scripts-mkswu
swupdate itself removes $TMPDIR/scripts at the end of update, but for
chained updates we will not want $MKSWU_TMP to be removed, so move the
scripts to another directory.

Also update relevant scripts (install_boot_linux/post_rootfs_baseos.sh)
to find rootdev and other state properly

Change-Id: I281c3963eec6330117fe7cdb7531cb936a56d457

scripts_pre.sh
@@ -13,7 +13,7 @@ if [ -n "$MKSWU_SKIP_SCRIPTS" ]; then
 fi
 
 TMPDIR="${TMPDIR:-/var/tmp}"
-MKSWU_TMP="$TMPDIR/scripts"
+MKSWU_TMP="$TMPDIR/scripts-mkswu"
 # SCRIPTSDIR is overridden for scripts embedded with swupdate
 SCRIPTSDIR="$MKSWU_TMP"
 

Makefile
@@ -146,7 +146,7 @@ install_swupdate:
 	sed -i -e 's/DEBUG_SKIP_SCRIPTS/DEBUG_SKIP_VENDORED_SCRIPTS/' $(DESTDIR)$(LIBEXEC)/common.sh
 	# modify scripts base directory
 	sed -i -e 's:^SCRIPTSDIR=.*:SCRIPTSDIR=$(LIBEXEC):' \
-		-e 's/^MKSWU_TMP=.*/&-vendored/' \
+		-e 's/^\(MKSWU_TMP=.*\)-mkswu/\1-vendored/' \
 		$(DESTDIR)$(LIBEXEC)/pre.sh \
 		$(DESTDIR)$(LIBEXEC)/post.sh \
 		$(DESTDIR)$(LIBEXEC)/cleanup.sh

mkswu
@@ -1179,7 +1179,7 @@ swdesc_boot_linux() {
 
 	eval "$MKSWU_export_swdesc_cmd_vars_stanza"
 	swdesc_exec_nochroot "$image" \
-		'${TMPDIR:-/var/tmp}/scripts/install_boot_linux $1'
+		'${TMPDIR:-/var/tmp}/scripts-mkswu/install_boot_linux $1'
 }
 
 swdesc_tar() {
@@ -1238,7 +1238,7 @@ swdesc_tar() {
 		MKSWU_component_baseos_seen="done"
 		MKSWU_hide_from_show=1 \
 			swdesc_command_nochroot --description "base_os post fixups" \
-				'${TMPDIR:-/var/tmp}/scripts/post_rootfs_baseos.sh'
+				'${TMPDIR:-/var/tmp}/scripts-mkswu/post_rootfs_baseos.sh'
 	fi
 }
 
@@ -1422,7 +1422,7 @@ swdesc_exec() {
 	*,base_os|*,extra_os*)
 		# run in container, writable OS
 		# use podman_target to use newly installed images
-		chroot_cmd='${TMPDIR:-/var/tmp}/scripts/podman_target '"$chroot_cmd"
+		chroot_cmd='${TMPDIR:-/var/tmp}/scripts-mkswu/podman_target '"$chroot_cmd"
 		chroot_cmd="$chroot_cmd --root /target/var/lib/containers"
 		chroot_cmd="$chroot_cmd -v /target:/target $container"
 		;;
@@ -1435,7 +1435,7 @@ swdesc_exec() {
 		;;
 	*)
 		# run in container, read-only OS, RW volumes
-		chroot_cmd='${TMPDIR:-/var/tmp}/scripts/podman_target '"$chroot_cmd"
+		chroot_cmd='${TMPDIR:-/var/tmp}/scripts-mkswu/podman_target '"$chroot_cmd"
 		chroot_cmd="$chroot_cmd -v /target/var/app/volumes:/target/var/app/volumes"
 		chroot_cmd="$chroot_cmd -v /target/var/app/rollback/volumes:/target/var/app/rollback/volumes"
 		chroot_cmd="$chroot_cmd -v /target:/target:ro $container"
@@ -1534,7 +1534,7 @@ swdesc_embed_container() {
 	container_check_archive
 
 	eval "$MKSWU_export_swdesc_cmd_vars_stanza"
-	swdesc_exec_nochroot "$image" '${TMPDIR:-/var/tmp}/scripts/podman_target load $1'
+	swdesc_exec_nochroot "$image" '${TMPDIR:-/var/tmp}/scripts-mkswu/podman_target load $1'
 }
 
 swdesc_pull_container() {
@@ -1543,7 +1543,7 @@ swdesc_pull_container() {
 
 	parse_swdesc pull_container "$@"
 
-	pull_cmd='${TMPDIR:-/var/tmp}/scripts/podman_target pull '
+	pull_cmd='${TMPDIR:-/var/tmp}/scripts-mkswu/podman_target pull '
 	pull_cmd="$pull_cmd ${container_tag:+"--tag \"$container_tag\" "} \"$image\""
 
 	eval "$MKSWU_export_swdesc_cmd_vars_stanza"
@@ -1568,7 +1568,7 @@ swdesc_usb_container() {
 	COPY_USB="$COPY_USB $(shell_quote "$(realpath "$OUTDIR/$image_usb.sig")")"
 
 	eval "$MKSWU_export_swdesc_cmd_vars_stanza"
-	swdesc_command_nochroot '${TMPDIR:-/var/tmp}/scripts/podman_target --pubkey /etc/swupdate.pem load '"$image_usb"
+	swdesc_command_nochroot '${TMPDIR:-/var/tmp}/scripts-mkswu/podman_target --pubkey /etc/swupdate.pem load '"$image_usb"
 }
 
 swdesc_sbom_source_file() {

scripts/cleanup.sh
@@ -4,7 +4,7 @@
 [ -n "$MKSWU_SKIP_SCRIPTS" ] && exit 0
 
 TMPDIR="${TMPDIR:-/var/tmp}"
-MKSWU_TMP="$TMPDIR/scripts"
+MKSWU_TMP="$TMPDIR/scripts-mkswu"
 # SCRIPTSDIR is overridden for scripts embedded with swupdate
 SCRIPTSDIR="$MKSWU_TMP"
 
@@ -23,9 +23,7 @@ if [ -n "$SWUPDATE_VERSION" ] \
 fi
 
 cleanup
-if [ "$MKSWU_TMP" != "$TMPDIR/scripts" ]; then
-	# swupdate removes TMPDIR/scripts itself, but we still
-	# need to remove the -vendored dir if it was used...
-	rm -rf "$MKSWU_TMP"
-fi
+# swupdate removes TMPDIR/scripts itself, but we still
+# need to remove mkswu's dir
+rm -rf "$MKSWU_TMP"
 unlock_update

scripts/common.sh
@@ -40,6 +40,7 @@ error() {
 	fi
 
 	cleanup ""
+	rm -rf "$MKSWU_TMP"
 	unlock_update
 	exit 1
 }

scripts/install_boot_linux
@@ -57,8 +57,8 @@ write_split_boot_partition() {
 }
 
 # state variable from mkswu scripts
-if [ -e "$TMPDIR/scripts/rootdev" ]; then
-	MKSWU_TMP="$TMPDIR/scripts"
+if [ -e "$TMPDIR/scripts-mkswu/rootdev" ]; then
+	MKSWU_TMP="$TMPDIR/scripts-mkswu"
 elif [ -e "$TMPDIR/scripts-vendored/rootdev" ]; then
 	MKSWU_TMP="$TMPDIR/scripts-vendored"
 else

scripts/podman_cleanup
@@ -181,8 +181,8 @@ podman_prune() {
 cleanup_images() {
 	local tmpdir
 
-	mkdir -p "${TMPDIR:-/var/tmp}/scripts"
-	tmpdir=$(mktemp -d "${TMPDIR:-/var/tmp}/scripts/mkswu_podman_cleanup.XXXXXX") \
+	mkdir -p "${TMPDIR:-/var/tmp}/scripts-mkswu"
+	tmpdir=$(mktemp -d "${TMPDIR:-/var/tmp}/scripts-mkswu/mkswu_podman_cleanup.XXXXXX") \
 		|| error "Could not create tmpdir in ${TMPDIR:-/var/tmp}"
 	trap "rm -rf '$tmpdir'" EXIT
 

scripts/podman_target
@@ -203,8 +203,8 @@ update_containers() {
 
 	# podman sometimes leaves lots of data in /var/tmp on errors:
 	# force a different TMPDIR and clean up after podman
-	mkdir -p "${TMPDIR:-/var/tmp}/scripts"
-	tmp=$(mktemp -d "${TMPDIR:-/var/tmp}/scripts/mkswu_podman_target.XXXXXX") \
+	mkdir -p "${TMPDIR:-/var/tmp}/scripts-mkswu"
+	tmp=$(mktemp -d "${TMPDIR:-/var/tmp}/scripts-mkswu/mkswu_podman_target.XXXXXX") \
 		|| error "Could not create tmpdir in ${TMPDIR:-/var/tmp}"
 	export TMPDIR="$tmp"
 	trap cleanup EXIT INT QUIT TERM

commit c55fa95f7710e7e096da5554e126c12777f3cd28

scripts: fix running cleanup script on failure
Moving the scripts to a different directory had the unintended
side-effect that swupdate could no longer run the embedded cleanup
script: it is expecting it to run from $TMPDIR/scripts/cleanup.sh but we
extracted it as $TMPDIR/scripts-mkswu/cleanup.sh

We could make swupdate look for the new location but for forward/backward
compatibility with swupdate it is better to create a link to it.

This had the side effect that /target was still mounted after errors
mid-update.

Also, running in verbose mode this was displayed:
[TRACE] : SWUPDATE running :  [mkswu_hook_cleanup] : Skipping non-executable cleanup_script /var/tmp/scripts/cleanup.sh

Fixes: 8fa267af487a ("scripts: move normal mkswu scripts dir to scripts-mkswu")
Change-Id: I58bd6988f5be904b63b4353f7aa7f6695f6752e8

scripts_pre.sh
@@ -62,6 +62,12 @@ fi
 # extract archive after script
 sed -e '1,/^BEGIN_ARCHIVE/d' "$0" | tar xv || exit 1
 
+# compat: when running from SWU, swupdate will try to run $TMPDIR/scripts/cleanup.sh,
+# but we moved to scripts-mkswu... Create a link for it
+if ! [ -e vendored ]; then
+	ln -sf ../scripts-mkswu/cleanup.sh "$TMPDIR/scripts/"
+fi
+
 # Allow skipping from env
 if [ -n "$MKSWU_SKIP_SCRIPTS" ]; then
 	echo "$0 skipping due to MKSWU_SKIP_SCRIPTS" >&2